707695e3736e7133ab0286549553bb00564409a4
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-06-26 13:08:44 -0700

    Time: 35 ms (55.5%), Space: 16.5 MB (63.41%) - LeetHub

---
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        # Brute Force
+        # TC: O(2N)
+
+        # temp = head
+        # length = 0
+
+        # while temp != None:
+        #     length += 1
+        #     temp = temp.next
+
+        # mid = length // 2
+
+
+        # temp = head
+        # pos = 0
+        # while pos < mid:
+        #     pos += 1
+        #     temp = temp.next
+        
+        # return temp
+
+        slow = head
+        fast = head
+
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+
+
+
+        
+        
