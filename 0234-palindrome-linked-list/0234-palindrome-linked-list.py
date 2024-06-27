f23a4763ccabd1889aac594a7c895a3c6bb62042
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-06-27 03:55:34 -0700

    Time: 337 ms (11.67%), Space: 36.5 MB (53.35%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -8,22 +8,71 @@ class Solution:
 
         # Brute Force:
         # TC: O(2N)
+        # SC: O(N)
+
+        # temp = head
+        # stack = []
+
+        # while temp != None:
+        #     stack.append(temp.val)
+        #     temp = temp.next
+
+        # temp = head
+        # while temp != None:
+        #     data = stack.pop(-1)
+        #     if temp.val != data:
+        #         return False
+        #     temp = temp.next
+
+        # return True
+
+        # Optimal: Slow and Fast Pointers
+        # TC: O(2N)
         # SC: O(1)
 
-        temp = head
-        stack = []
+        if head == None or head.next == None:
+            return head
 
-        while temp != None:
-            stack.append(temp.val)
-            temp = temp.next
+        slow = head
+        fast = head
 
-        temp = head
-        while temp != None:
-            data = stack.pop(-1)
-            if temp.val != data:
+        while fast.next != None and fast.next.next != None: # TC: O(N/2)
+            slow = slow.next
+            fast = fast.next.next
+
+        newHead = self.reverse(slow.next) # TC: O(N/2)
+
+        first = head
+        second = newHead
+
+        while second != None:     # TC: O(N/2)
+            if first.val != second.val:
+                self.reverse(newHead)
                 return False
-            temp = temp.next
+            first = first.next
+            second = second.next
 
+        self.reverse(newHead)  # TC: O(N/2)
+        
         return True
+
+        
+
+    def reverse(self, head):
+        temp = head
+        prev = None
+        while temp != None:
+            nextNode = temp.next
+            temp.next = prev
+            prev = temp
+            temp = nextNode
+
+        return prev
+
+        
+
+
+
+
         
         
