758a81fa2145dc9f38d418c4917838ed28b114cd
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-06-30 02:43:48 -0700

    Time: 44 ms (53.28%), Space: 18.8 MB (95.39%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -7,23 +7,51 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # Brute Force: Hashing
+        # TC: O(N)
+        # SC: O(N)
+
+        # if head == None or head.next == None:
+        #     return None
+
+        # hashMap = defaultdict(ListNode)
+        # temp = head
+        # pos = 0 # 0-indexed(given)
+
+        # while temp != None:
+        #     if temp in hashMap:
+        #         return temp
+
+        #     hashMap[temp] = pos
+        #     pos += 1
+        #     temp = temp.next
+
+        # return None
+
+        # d = distance from node cycle begins to fast == slow
+        # l1 = distance from the start of the LL to the first node of cycle
+        # Length of the loop = d + l1
+
+        # Optimal: Slow and Fast Pointer
 
         if head == None or head.next == None:
             return None
 
-        hashMap = defaultdict(ListNode)
-        temp = head
-        pos = 0 # o-indexed(given)
-
-        while temp != None:
-            if temp in hashMap:
-                return temp
-
-            hashMap[temp] = pos
-            pos += 1
-            temp = temp.next
+        slow = head
+        fast = head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
 
         return None
+
+
+        
 
 
         
