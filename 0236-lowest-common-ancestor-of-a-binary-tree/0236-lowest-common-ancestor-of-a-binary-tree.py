d4d84c89be7c6d21d8978aed288e5d7dc97aaba9
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-14 09:15:04 -0700

    Time: 50 ms (59.35%), Space: 21.1 MB (16.99%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -11,34 +11,53 @@ class Solution:
         # TC: O(N) + O(N) + (N) = O(3N)
         # SC: O(N) + O(N) = O(2N)
 
-        pathP = []
-        pathQ = []
-        self.getPath(root, p, pathP)
-        self.getPath(root, q, pathQ)
+        # pathP = []
+        # pathQ = []
+        # self.getPath(root, p, pathP)
+        # self.getPath(root, q, pathQ)
 
-        prevCommom = TreeNode(-1)
-        length = min(len(pathP), len(pathQ))
-        for i in range(length):
-            if pathP[i] == pathQ[i]:
-                prevCommom = pathP[i]
+        # prevCommom = TreeNode(-1)
+        # length = min(len(pathP), len(pathQ))
+        # for i in range(length):
+        #     if pathP[i] == pathQ[i]:
+        #         prevCommom = pathP[i]
 
-        return prevCommom
+        # return prevCommom
+
+        # Approach 2
+        # TC: O(N)
+        # SC: O(N)
+
+        # Base Case
+        if root == None or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q) # Go left
+        right = self.lowestCommonAncestor(root.right, p, q) # Go right
+
+        # Result 
+        if left == None:
+            return right
+        elif right == None:
+            return left
+        else: # Both left and right are not null, we found our result
+            return root
             
 
-    def getPath(self, root, targetNode, res):
-        if root == None:
-            return False
+    # def getPath(self, root, targetNode, res):
+    #     if root == None:
+    #         return False
 
-        res.append(root)
+    #     res.append(root)
 
-        if root == targetNode:
-            return True
+    #     if root == targetNode:
+    #         return True
 
-        if self.getPath(root.left, targetNode, res) or self.getPath(root.right, targetNode, res):
-            return True
+    #     if self.getPath(root.left, targetNode, res) or self.getPath(root.right, targetNode, res):
+    #         return True
 
-        res.pop(-1)
+    #     res.pop(-1)
 
-        return False
+    #     return False
 
         
