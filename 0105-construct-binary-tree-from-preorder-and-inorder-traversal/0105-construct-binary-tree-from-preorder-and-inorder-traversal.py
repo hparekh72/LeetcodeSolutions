07185c3d10deba918d1aba4be9bfc8575df6054d
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-17 13:54:06 -0700

    Time: 47 ms (94.26%), Space: 18.4 MB (73.44%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -11,16 +11,61 @@ class Solution:
         # TC: O(N^2)
         # SC: O(N) (recursive call stack) + O(N) (new preorder and inorder arrays in each call) = O(N)
 
-        if len(preorder) == 0 or len(inorder) == 0:
+        # if len(preorder) == 0 or len(inorder) == 0:
+        #     return None
+
+        # root = TreeNode(preorder[0])
+        # mid = inorder.index(preorder[0]) # TC: O(N)
+
+        # root.left = self.buildTree(preorder[1 : mid+1], inorder[ : mid]) # Skipping 0-index in preorder, as we already just created a node 
+        # root.right = self.buildTree(preorder[mid+1 : ], inorder[mid+1 : ])
+
+        # return root
+
+        # Optimal:
+        # TC: O(N)
+        # SC: O(N) + O(N) = O(N)
+
+        inorder_map = {} # SC: O(N)
+        for index, value in enumerate(inorder):
+            # Create a map to store indices of elements in the inorder traversal
+            inorder_map[value] = index
+
+        return self.createTree(preorder, 0, len(preorder) - 1, inorder, 0, len(inorder) - 1, inorder_map)
+
+    def createTree(self, preorder, preStart, preEnd, inorder, inStart, inEnd, inorder_map):
+        # Base case: If the start indices exceed the end indices, return None
+        if preStart > preEnd or inStart > inEnd:
             return None
 
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0]) # TC: O(N)
+        # Create a new TreeNode with value at the current preorder index
+        root = TreeNode(preorder[preStart])
 
-        root.left = self.buildTree(preorder[1 : mid+1], inorder[ : mid]) # Skipping 0-index in preorder, as we already just created a node 
-        root.right = self.buildTree(preorder[mid+1 : ], inorder[mid+1 : ])
+        # Find the index of the current root value in the inorder traversal
+        mid = inorder_map[root.val]
+
+        # Calculate the number of elements in the left subtree
+        numsLeft = mid - inStart
+
+        # Recursively build the left subtree
+        root.left = self.createTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, mid - 1, inorder_map)
+
+        # Recursively build the right subtree
+        root.right = self.createTree(preorder, preStart + numsLeft + 1, preEnd, inorder, mid + 1, inEnd, inorder_map)
 
         return root
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
         
