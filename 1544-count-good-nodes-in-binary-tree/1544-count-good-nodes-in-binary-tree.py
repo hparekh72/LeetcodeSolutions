7bd75c109eb4e79bdabc60fe0a2e51a5fab64ec8
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-18 07:31:09 -0700

    Time: 131 ms (59.46%), Space: 31.5 MB (8.59%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -4,6 +4,32 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def goodNodes(self, root: TreeNode) -> int:
+#         # TC: O(N)
+#         # SC: O(N)
+#         if root == None:
+#             return 
+
+#         maximum = root.val
+#         count = [0]
+#         self.countGoodNodes(root, maximum, count)
+#         return count[0]
+
+#     # Preorder Traversal
+#     def countGoodNodes(self, root, maximum, count):
+
+#         if root == None:
+#             return
+
+#         if root.val >= maximum:
+#             maximum = root.val
+#             count[0] += 1
+
+#         self.countGoodNodes(root.left, maximum, count)
+#         self.countGoodNodes(root.right, maximum, count) 
+
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         # TC: O(N)
@@ -11,22 +37,22 @@ class Solution:
         if root == None:
             return 
 
-        maximum = root.val
-        count = [0]
-        self.countGoodNodes(root, maximum, count)
-        return count[0]
+        return self.countGoodNodes(root, root.val)
+ 
 
-    # Preorder Traversal
-    def countGoodNodes(self, root, maximum, count):
+    # Preorder Traversal (DFS)
+    def countGoodNodes(self, root, maximum):
 
         if root == None:
-            return
+            return 0
 
         if root.val >= maximum:
             maximum = root.val
-            count[0] += 1
+            res = 1
+        else:
+            res = 0
 
-        self.countGoodNodes(root.left, maximum, count)
-        self.countGoodNodes(root.right, maximum, count)
-
+        res += self.countGoodNodes(root.left, maximum)
+        res += self.countGoodNodes(root.right, maximum)
+        return res
         
