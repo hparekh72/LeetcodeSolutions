0761b5f7d85d5b1a65ca6da6e2be5b98f7951457
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-19 08:47:36 -0700

    Time: 94 ms (6.07%), Space: 18.8 MB (14.86%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.py
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.py
@@ -8,21 +8,52 @@ class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         # TC: O(logN) N -> Number of nodes
         # SC: O(logN)
+        # Intution: Find where(always a leaf node) it can be and insert
 
+
+        # Recursive Approach
+
+        # if root == None: # If empty tree, create root node
+        #     root = TreeNode(val)
+        #     return root
+
+        # if val < root.val: 
+        #     if root.left == None:
+        #         root.left = TreeNode(val)
+        #     else:
+        #         self.insertIntoBST(root.left, val)
+        # else:
+        #     if root.right == None:
+        #         root.right = TreeNode(val)
+        #     else:
+        #         self.insertIntoBST(root.right, val)
+
+        # return root
+
+        #Iterative Approach
+        # TC: O(logN) N -> Number of nodes
+        # SC: O(1)
         if root == None: # If empty tree, create root node
             root = TreeNode(val)
             return root
 
-        if val < root.val: 
-            if root.left == None:
-                root.left = TreeNode(val)
+        curr = root
+        while True:
+            if val < curr.val: 
+                if curr.left == None:
+                    curr.left = TreeNode(val)
+                    break
+                else:
+                    curr = curr.left
             else:
-                self.insertIntoBST(root.left, val)
-        else:
-            if root.right == None:
-                root.right = TreeNode(val)
-            else:
-                self.insertIntoBST(root.right, val)
+                if curr.right == None:
+                    curr.right = TreeNode(val)
+                    break
+                else:
+                    curr = curr.right
 
         return root
+
+
+
         
