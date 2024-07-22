4dc304e9a74e76efc8493a6100a0c973d932d6e6
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-22 03:14:24 -0700

    Time: 36 ms (86.56%), Space: 18.4 MB (31.86%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -4,29 +4,58 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+#         # TC: O(N)
+#         #SC: O(N) Recursive Stack Space
+#         res = [True]
+#         prev = [None]
+#         self.inorder(root, prev, res)
+#         return res[0]
+
+#     def inorder(self, root, prev, res):
+#         if root == None:
+#             return 
+
+#         self.inorder(root.left, prev, res)
+
+#         # Check if the current node's value is greater than the previous node's value
+#         if prev[0] != None and prev[0] >= root.val:
+#             res[0] = False
+#             return
+#         # Update the prev value to the current node's value
+#         prev[0] = root.val
+
+#         self.inorder(root.right, prev, res)
+
+# Approach 2:
+# TC: O(N)
+# SC: O(N)
+
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # TC: O(N)
-        #SC: O(N) Recursive Stack Space
-        res = [True]
-        prev = [None]
-        self.inorder(root, prev, res)
-        return res[0]
+        # Use a helper function that can use early exit
+        # 'prev' is used as a mutable container to track the last node value
+        self.prev = None
+        return self.inorder(root)
 
-    def inorder(self, root, prev, res):
-        if root == None:
-            return 
+    def inorder(self, root):
+        if root is None:
+            return True
+        
+        # Traverse the left subtree
+        if not self.inorder(root.left):
+            return False
 
-        self.inorder(root.left, prev, res)
+        # Check current node value with the previous node value
+        if self.prev is not None and self.prev >= root.val:
+            return False
+        # Update the prev pointer
+        self.prev = root.val
 
-        # Check if the current node's value is greater than the previous node's value
-        if prev[0] != None and prev[0] >= root.val:
-            res[0] = False
-            return
-        # Update the prev value to the current node's value
-        prev[0] = root.val
+        # Traverse the right subtree
+        return self.inorder(root.right)
 
-        self.inorder(root.right, prev, res)
 
         
         
