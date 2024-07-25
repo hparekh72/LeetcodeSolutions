a34e8337ab346e2821d6c357bc1131b2a395973b
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-25 10:54:49 -0700

    Time: 37 ms (79.07%), Space: 16.7 MB (55.49%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,36 +1,67 @@
+
+"""
+Brute Force:
+TC: O(n! * n(looping)) 
+SC: O(n) (Recursive stack space) + O(n) (ds array) + O(n)(visited array) + O(n!)(res array)
+"""
+
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+
+#         res = []
+#         visited = [False]*len(nums)
+#         self.getPermutations(nums, visited, [], res)
+#         return res
+
+#     def getPermutations(self, nums, visited, ds, res):
+
+#         # Base Case
+#         if len(ds) == len(nums):
+#             res.append(ds.copy())
+#             return
+
+
+#         for i in range(len(nums)):
+#             if not visited[i]:
+
+#                 visited[i] = True
+#                 ds.append(nums[i])
+
+#                 self.getPermutations(nums, visited, ds, res)
+
+#                 visited[i] = False
+#                 ds.pop()
+
+"""
+Optimal: Swapping
+TC: O(n! * n(looping)) 
+SC: O(n) (Recursive stack space) + O(n!)(res array)
+"""
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-
-        # Brute Force:
-        # TC: O(n! * n(looping)) 
-        # SC: O(n) (Recursive stack space) + O(n) (ds array) + O(n)(visited array)
-
         res = []
-        visited = [False]*len(nums)
-        self.getPermutations(nums, visited, [], res)
+        self.getPermutations(nums, 0, res)
         return res
 
-    def getPermutations(self, nums, visited, ds, res):
+    def swap(self, i, j, nums):
+        temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
 
-        # Base Case
-        if len(ds) == len(nums):
-            res.append(ds.copy())
-            return
+    def getPermutations(self, nums, index, res):
 
+        # Base Case:
+        if index == len(nums):
+            res.append(nums.copy())
+            return 
 
-        for i in range(len(nums)):
-            if not visited[i]:
-
-                visited[i] = True
-                ds.append(nums[i])
-
-                self.getPermutations(nums, visited, ds, res)
-
-                visited[i] = False
-                ds.pop()
+        for i in range(index, len(nums)):
+            self.swap(index, i, nums)
+            self.getPermutations(nums, index + 1, res)
+            self.swap(index, i, nums) # Backtrack
 
 
-            
 
 
 
