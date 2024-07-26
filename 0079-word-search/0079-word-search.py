5ccb1ca8d6e754bf7c0aebdf64d33626bb473287
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-26 05:38:58 -0700

    Time: 3924 ms (62.37%), Space: 16.5 MB (83.26%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,44 @@
+class Solution:
+
+    # TC: O(n * m * 4l(dfs)) 
+    # Here, n * m are the board dimensions, l is the length of the word
+
+    # SC: O(l) (recursive call stack) + O(l) (set)
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, ind):
+            # Base Case
+            if ind == len(word): # Word has found
+                return True
+
+            # If current position out of grid boundaries, or character does not match or already visited
+            if (r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != word[ind] or (r, c) in path):
+                return False
+
+            # Character matches
+            path.add((r, c)) # Mark the current position to the set as visited
+
+            # Recursively explore all four directions
+            res = (
+                dfs(r+1, c, ind+1) or # Up
+                dfs(r-1, c, ind+1) or # Down
+                dfs(r, c-1, ind+1) or # Left
+                dfs(r, c+1, ind+1) # Right
+            )
+
+            path.remove((r, c)) # Reset Path
+
+            return res
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+
+        return False
+
+
+        
