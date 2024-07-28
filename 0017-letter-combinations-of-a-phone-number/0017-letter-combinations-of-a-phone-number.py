3195d020b016914d3a6e035521726c940ac67b3c
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-28 12:55:35 -0700

    Time: 34 ms (67.08%), Space: 16.5 MB (30.21%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,32 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        # TC: (4 ^ n) * n (worst case)
+        # SC: o(n) (dictionary) +  O(len(digits)) + (4 ^ n)(output array) (recursive stack space) 
+
+        res = []
+        digitsToChar = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"
+        }
+
+        def backtrack(ind, currStr):
+            if len(currStr) == len(digits):
+                res.append(currStr)
+                return
+
+            for c in digitsToChar[digits[ind]]:
+                backtrack(ind + 1, currStr + c)
+
+        if digits:
+            backtrack(0, "")
+        
+        return res
+
+        
