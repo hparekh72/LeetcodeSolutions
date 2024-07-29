569f27c981635bf4e9d88dfad603814f430c9d2f
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-29 12:59:43 -0700

    Time: 322 ms (5.14%), Space: 18.7 MB (10.27%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -4,6 +4,35 @@ class Solution:
         # Brute Force: Sorting
         # TC: O(nlogn) + O(nlogn)
 
-        closest_sorted_arr = sorted(arr, key=lambda num: (abs(x - num), num))
+        # closest_sorted_arr = sorted(arr, key=lambda num: (abs(x - num), num))
         
-        return sorted(closest_sorted_arr[:k])
+        # return sorted(closest_sorted_arr[:k])
+
+        # Optimal: Using Max-heap
+        # TC: O(nlogk) + O(klogk)
+        # SC: O(k)
+
+
+
+        heap = []
+
+        for i in range(len(arr)):
+             # Max-heap
+             # Negative key for reverse order on tie as default value to pop in a tie is lexicographically smaller
+            heapq.heappush(heap, (-abs(arr[i] - x), -arr[i]))
+
+            if len(heap) > k: 
+                heapq.heappop(heap)
+
+        res = []
+        while heap:
+            _, key = heapq.heappop(heap)
+            key = -key
+            res.append(key)
+
+        return sorted(res)
+
+
+        
+
+            
