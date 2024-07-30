5ad4b86c2191265a6fbee6ee5f47e1704c26c758
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-30 13:35:24 -0700

    Time: 74 ms (83.06%), Space: 20.8 MB (34.78%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -1,12 +1,19 @@
 class KthLargest:
 
-    # TC: O(nlogk)
+    # Brute Force: Sort the array 
+    # TC: __init__(): O(nlogn)
+    # TC: add(): O(n) (To add an element at a specific position in an sorted array)
+    # SC: O(n)
+
+    # Optimal: Min Heap 
+    # TC: __init__(): O(nlogk)
+    # TC: add(): O(logk) (To push and pop an element in the heap)
     # SC: O(k)
 
-    def __init__(self, k: int, nums: List[int]):
+    def __init__(self, k: int, nums: List[int]): # O(nlogk)
         self.heap = []
         self.k = k
-        
+
         for num in nums:
             heapq.heappush(self.heap, num) # Min Heap
 
@@ -16,10 +23,10 @@ class KthLargest:
         return None
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.heap, val)
+        heapq.heappush(self.heap, val) # O(logk)
 
         if len(self.heap) > self.k:
-            heapq.heappop(self.heap)
+            heapq.heappop(self.heap) # O(logk)
 
         return self.heap[0]
         
