6ae52c313a955f97133a0d466e75175d5bfe2a3a
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-07-31 05:52:33 -0700

    Time: 438 ms (89.89%), Space: 29.6 MB (74.17%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -17,6 +17,29 @@ class Solution:
 
         return heap[0]
 
+        # Approach 3: QuickSelect Algorithm
+        # TC: Worst-case: O(n^2)
+            # Average-case O(n)
+
+        # kthIndex = len(nums) - k
+
+        # def quickSelect(l, r):
+        #     pivot, p = nums[r], l
+        #     for i in range(l, r):
+        #         if nums[i] <= pivot:
+        #             nums[p], nums[i] = nums[i], nums[p] # Swap
+        #             p += 1
+        #     nums[p], nums[r] = nums[r], nums[p] # Swap
+
+        #     if p < kthIndex: 
+        #         return quickSelect(p+1, r) # Right
+        #     elif p > kthIndex:
+        #         return quickSelect(l, p-1) # Left
+        #     else: 
+        #         return nums[p] # Answer
+
+        # return quickSelect(0, len(nums) - 1)
+
 
         
 
