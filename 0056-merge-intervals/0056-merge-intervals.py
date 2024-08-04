087124181716bf59f4509ae151fdd8b6abd29007
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-04 10:44:20 -0700

    Time: 127 ms (37.58%), Space: 20.6 MB (45.86%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,25 @@
+# Brute Force:
+# TC: O(nlogn) + O(n)
+# SC: O(n)
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals.sort()
+
+        res = []
+        previousInterval = intervals[0]
+
+        for i in range(1, len(intervals)):
+            if previousInterval[1] >= intervals[i][0]:
+                previousInterval[1] = max(previousInterval[1], intervals[i][1])
+            else:
+                res.append(previousInterval)
+                previousInterval = intervals[i]
+        
+        res.append(previousInterval)
+
+        return res
+
+        
