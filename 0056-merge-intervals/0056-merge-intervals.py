ae431c1f8bd667cd7de9a17bd73d2899656b2369
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-04 11:35:45 -0700

    Time: 121 ms (68.98%), Space: 20.6 MB (45.86%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,8 +1,12 @@
 # Brute Force:
+# TC: O(nlogn) + O(2n)
+# SC: O(n)
+
+#  Optimal:
 # TC: O(nlogn) + O(n)
 # SC: O(n)
 
-
+#  Optimal:
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
