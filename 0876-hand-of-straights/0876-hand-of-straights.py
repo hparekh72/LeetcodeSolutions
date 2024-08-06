2857619fc66b35c1c88568e1bd71cc4020d8413f
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-06 11:20:23 -0700

    Time: 151 ms (90.17%), Space: 18.6 MB (11.19%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,34 +1,67 @@
+# Optimal: Using Greedy Approach
+# TC: O(nlogn) + O(n)
+# SC: O(n)
+
+# class Solution:
+#     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+
+#         if len(hand) % groupSize:
+#             return False
+
+#         hand.sort()
+        
+#         hashMap = defaultdict(int)
+
+#         for i in range(len(hand)):
+#             hashMap[hand[i]] += 1
+
+#         while hashMap:
+
+#             start = next(iter(hashMap))
+#             for i in range(start, start + groupSize):
+
+#                 if i not in hashMap:
+#                     return False
+
+#                 hashMap[i] -= 1
+#                 if hashMap[i] == 0:
+#                     del hashMap[i]
+
+#         return True
+
+# Optimal: Using Greedy Approach
+# Similar approach as previous code, instead of sorting, we are using Min-Heap
 # TC: O(nlogn) + O(n)
 # SC: O(n)
 
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-
         if len(hand) % groupSize:
             return False
-
-        hand.sort()
         
-        hashMap = defaultdict(int)
+        hashMap = {}
+        for num in hand:
+            hashMap[num] = 1 + hashMap.get(num, 0)
 
-        for i in range(len(hand)):
-            hashMap[hand[i]] += 1
+        minHeap = list(hashMap.keys())
+        heapq.heapify(minHeap)
 
-        while hashMap:
+        while minHeap:
 
-            start = next(iter(hashMap))
+            start = minHeap[0]
             for i in range(start, start + groupSize):
-
                 if i not in hashMap:
                     return False
-
+                    
                 hashMap[i] -= 1
                 if hashMap[i] == 0:
-                    del hashMap[i]
+                    # Only remove from heap if it's the current smallest element
+                    # Ensures we don't pop out of order, which could miss a card
+                    if i != minHeap[0]:
+                        return False
+                    heapq.heappop(minHeap)
 
         return True
-
-
 
 
 
