739a7c3e9bcb65263ca0f9c2e24a462c1c13b819
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-06 08:53:47 -0700

    Time: 188 ms (27.63%), Space: 18.2 MB (51.52%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -0,0 +1,34 @@
+# TC: O(nlogn) + O(n)
+# SC: O(n)
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        hand.sort()
+        
+        hashMap = defaultdict(int)
+
+        for i in range(len(hand)):
+            hashMap[hand[i]] += 1
+
+        while hashMap:
+
+            start = next(iter(hashMap))
+            for i in range(start, start + groupSize):
+
+                if i not in hashMap:
+                    return False
+                    
+                hashMap[i] -= 1
+                if hashMap[i] == 0:
+                    del hashMap[i]
+
+        return True
+
+
+
+
+        
+
+        
+
+          
