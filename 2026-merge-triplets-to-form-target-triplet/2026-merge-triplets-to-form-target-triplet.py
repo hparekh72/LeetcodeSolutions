e16c2ac9b3aaa5be295eeac84acb491cfcd96396
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-06 12:20:14 -0700

    Time: 1587 ms (70.16%), Space: 61.7 MB (57.35%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
@@ -1,6 +1,6 @@
 # Optimal
-# TC: O(n)
-# SC: O(1)
+# TC: O(n) where n is the number of triplets in the input list.
+# SC: O(1) 
 
 # Approach: Selectively consider only those triplets that do not exceed target values and tracks indices where the target values are matched exactly.
 
@@ -12,7 +12,7 @@ class Solution:
             if (t[0] > target[0]) or (t[1] > target[1]) or (t[2] > target[2]):
                 continue
             
-            for index, value in enumerate(t):
+            for index, value in enumerate(t): # TC: O(1) (constant time due to the fixed size of three)
                 if value == target[index]:
                     good.add(index)
 
