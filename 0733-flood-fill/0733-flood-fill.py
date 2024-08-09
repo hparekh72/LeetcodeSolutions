310c117c0d0433892bfb8e489dab4f13c899e8a0
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-09 09:59:36 -0700

    Time: 68 ms (49.09%), Space: 16.6 MB (88.1%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,19 +1,41 @@
 # BFS Implementation
 # TC: O(4 * N * M) 
-# SC: O(N * M) (visited array) + O(N * M) (queue)
+# SC: O(N * M) (queue)
+
+# DFS Implementation
+# TC: O(4 * N * M) 
+# SC: O(N * M) (recursion stack space)
 
 
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
+        # return self.bfs(image, sr, sc, color)
+        startColor = image[sr][sc]
+        self.dfs(image, sr, sc, color, startColor)
+        return image
+
+    def dfs(self, image, row, col, color, startColor):
+
+        # Base Condition
+        if row < 0 or row >= len(image) or col < 0 or col >= len(image[0]) or image[row][col] != startColor or image[row][col] == color:
+            return 
+        
+        image[row][col] = color # Replace with new color
+
+        self.dfs(image, row - 1, col, color, startColor) # Up
+        self.dfs(image, row, col + 1, color, startColor) # Right
+        self.dfs(image, row + 1, col, color, startColor) # Down
+        self.dfs(image, row, col - 1, color, startColor) # Left
+
+
+    def bfs(self, image, sr, sc, color):
         rows = len(image)
         cols = len(image[0])
         currentColor = image[sr][sc]
-        visited = [[0 for _ in range(cols)] for _ in range(rows)]
 
         queue = deque([(sr, sc)])
         image[sr][sc] = color
-        visited[sr][sc] = 1
 
 
         # Up, Down, Left, Right
@@ -27,14 +49,12 @@ class Solution:
                 row = r + dRow[i]
                 col = c + dColumn[i]
 
-                if row < 0 or row >= rows or col < 0 or col >= cols or visited[row][col] == 1 or image[row][col] != currentColor:
+                if row < 0 or row >= rows or col < 0 or col >= cols or image[row][col] == color or image[row][col] != currentColor:
                     continue
                 else:
                     queue.append([row, col])
-                    image[row][col] = color
-                    visited[row][col] = 1
+                    image[row][col] = color # Replace with new color
         
-        return image
 
 
 
