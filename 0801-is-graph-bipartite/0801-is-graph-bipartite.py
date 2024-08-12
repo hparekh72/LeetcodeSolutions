4ad82cfc6f762fad80e2adb0735caf3f067c724a
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-12 09:57:42 -0700

    Time: 148 ms (43.55%), Space: 17.1 MB (42.41%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -7,11 +7,11 @@ class Solution:
         color = [-1] * len(graph)
         for i in range(len(graph)):
             if color[i] == -1:
-                # if self.bfs(i, graph, color) == False:
-                #     return False
-
-                if self.dfs(i, 0, graph, color) == False:
+                if self.bfs(i, graph, color) == False:
                     return False
+
+                # if self.dfs(i, 0, graph, color) == False:
+                #     return False
         return True
 
     def bfs(self, start, graph, color): # TC: O(N + 2E)
@@ -37,9 +37,11 @@ class Solution:
         colorArr[node] = color
 
         for neighbor in graph[node]:
+            # If the neighbor(adjacent) node is not yet colored, we will give the opposite color of the node
             if colorArr[neighbor] == -1:
                 if self.dfs(neighbor, not color, graph, colorArr) == False:
                     return False
+            # If the neighbor(adjacent) node is having the same color as the node, then someone did color it on some other path
             elif colorArr[neighbor] == color:
                 return False
             
