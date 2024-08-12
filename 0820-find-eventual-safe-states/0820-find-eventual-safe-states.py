09a1ee0a4c0469a35ee1ad78d6cbd307d54ca23e
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-12 13:24:49 -0700

    Time: 485 ms (99.72%), Space: 24 MB (78.62%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -0,0 +1,45 @@
+# Intution: All possible paths starting from a node are going to end at some terminal node unless there exists a cycle and the paths return back to themselves. So, the intuition is to figure out the nodes which are neither a part of a cycle nor connected to the cycle.
+
+# TC: O(N) + O(N + E)
+# SC: O(3N) + O(N) (dfs recursive stack space) ~ O(N)
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        V = len(graph)
+        visited, pathVisited, check = [0] * V, [0] * V, [0] * V
+
+        for i in range(V):
+            if visited[i] == 0:
+                self.dfs(i, graph, visited, pathVisited, check)
+
+        safeNodes = []
+        for i in range(V):
+            if check[i] == 1:
+                safeNodes.append(i)
+
+        return safeNodes
+        
+
+    def dfs(self, node, graph, visited, pathVisited, check): # TC: O(N + E)
+        visited[node] = 1
+        pathVisited[node] = 1
+
+        # Traverse for adjacent nodes
+        for neighbor in graph[node]:
+            # When the node is not visited
+            if visited[neighbor] == 0:
+                if self.dfs(neighbor, graph, visited, pathVisited, check) == True:
+                    return True
+            
+            # If the node has been previously visited but it has to be visited on the same path
+            elif pathVisited[neighbor]:
+                return True
+
+        # Ie the node is neither a part of a cycle nor connected to the cycle.
+        check[node] = 1
+        pathVisited[node] = 0
+        return False
+
+
+
+        
