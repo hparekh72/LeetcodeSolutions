80ec84436a0d584e785afdf8d2b6b60d27016c19
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-16 09:42:27 -0700

    Time: 91 ms (53.91%), Space: 18.3 MB (59.44%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,31 +1,31 @@
 # Topological Sort
-# TC: O(V + E) + (V) (reverse), V = number of courses
-# SC: O(V)
+# TC: O(V + E), V = number of courses, E = number of edges(prerequisites)
+# SC: O(V + E) (adjacency list & indegree) + O(V) (queue & result) ~ O(V)
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
-        # Create an adjacency list
-        pre = defaultdict(list)
+        # Create an adjacency list # TC: O(E)
+        pre = defaultdict(list) 
         for course, prerequisite in prerequisites:
-            pre[course].append(prerequisite)
+            pre[prerequisite].append(course)        
+
+        # Calculate the indegree for each node # TC: O(E)
+        indegree = [0] * numCourses
+        for course, prerequisite in prerequisites:
+            indegree[course] += 1
 
         # Topological Sort
-
         queue = deque()
-        indegree = [0] * numCourses
-
-        for node in pre:
-            for neighbor in pre[node]:
-                indegree[neighbor] += 1
 
         # Add the nodes in the queue whose indegree is 0
         for i in range(len(indegree)):
             if indegree[i] == 0:
                 queue.append(i)
 
+        
         res = []
-        while queue:
+        while queue: # TC: O(V + E)
             node = queue.popleft()
             res.append(node)
 
@@ -36,17 +36,9 @@ class Solution:
                     queue.append(neighbor)
         
         if len(res) == numCourses:
-            self.reverse(res)
             return res
         else:
             return []
 
-    def reverse(self, res):
-        i, j = 0, len(res) - 1
-        while i < j:
-            print(res[i], res[j])
-            res[i], res[j] = res[j], res[i]
-            i += 1
-            j -= 1
         
             
