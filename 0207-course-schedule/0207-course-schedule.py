936ccc7d64fe6da3d002f0dfeb8e9cb3d9120c80
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-16 08:13:07 -0700

    Time: 93 ms (38.04%), Space: 18.3 MB (48.38%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,7 +1,11 @@
-# Topological Sort 
-# TC: O(V + E)
-# SC: O(number of courses)
+# Note: Can solve the question using 2 ways
+# 1) Detect if a directed graph has a cycle (using DFS)
+# 2) Topological Sort (using BFS)
 
+
+# Topological Sort 
+# TC: O(V + E), V = number of courses
+# SC: O(number of courses)
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
@@ -10,6 +14,8 @@ class Solution:
         pre = defaultdict(list)
         for course, prerequisite in prerequisites:
             pre[course].append(prerequisite)
+
+        # Topological Sort
 
         queue = deque()
         indegree = [0] * numCourses
@@ -24,19 +30,23 @@ class Solution:
             if indegree[i] == 0:
                 queue.append(i)
 
+        courses = 0
         while queue:
             node = queue.popleft()
+            courses += 1
+
             for neighbor in pre[node]:
                 indegree[neighbor] -= 1
 
                 if indegree[neighbor] == 0:
                     queue.append(neighbor)
 
-        for i in range(len(indegree)):
-            if indegree[i] > 0: # If there is a cycle in the graph
-                return False
-        
-        return True
+        if courses == numCourses:
+            return True
+        else:
+            return False
+
+
 
             
 
