60f405b90bf48ad788d353c5e8b11888db8e6300
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-08-28 08:27:54 -0700

    Time: 44 ms (28.73%), Space: 16.8 MB (73.99%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -14,30 +14,61 @@ class Node:
 #4. return oldToNew[node]
 
 # Approach 2: Optimal:
-# TC: O(V + E) (dfs traversal)
+# TC: O(V + E) (dfs or bfs traversal)
 # SC: O(V) (dictionary and recursion call stack)
 
 
 from typing import Optional
+# class Solution:
+#     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+#         oldToNew = {} # Map which stores "Old node address: New Node address"
+
+#         def dfs(node):
+#             if node in oldToNew:
+#                 return oldToNew[node]
+
+#             copyNode = Node(node.val)
+#             oldToNew[node] = copyNode
+
+#             for neighbor in node.neighbors:
+#                 copyNode.neighbors.append(dfs(neighbor))
+
+#             return copyNode
+
+#         if node:
+#             return dfs(node)
+#         else:
+#             return None
+
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        oldToNew = {}
-        def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
-
-            copyNode = Node(node.val)
-            oldToNew[node] = copyNode
-
-            for neighbor in node.neighbors:
-                copyNode.neighbors.append(dfs(neighbor))
-
-            return copyNode
-
-        if node:
-            return dfs(node)
-        else:
+        # If give node is None
+        if node == None:
             return None
+
+        queue = deque()
+        oldToNew = {} # Map which stores "Old node address: New Node address"
+        
+        # Putting given node in the queue
+        queue.append(node)
+
+        # Map the old node to the new(copy) node
+        oldToNew[node] = Node(node.val)
+
+        while queue:
+            old_node = queue.popleft() # Current Node
+
+            # Traverse the Neighbours of old_node (current node)
+            for neighbor in old_node.neighbors:
+                # If Neighbour is Not in Map
+                if neighbor not in oldToNew:
+                    oldToNew[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+
+                # Linking
+                oldToNew[old_node].neighbors.append(oldToNew[neighbor])
+
+        return oldToNew[node]
 
         # Approach 3: Pythonic way
         # return copy.deepcopy(node)
