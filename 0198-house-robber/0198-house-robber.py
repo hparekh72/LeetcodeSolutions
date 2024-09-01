e099fd93b425fefff54b9bd6bb84b755867f0932
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-09-01 14:21:35 -0700

    Time: 45 ms (7.01%), Space: 16.5 MB (69.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,7 +5,8 @@ class Solution:
 
         # return self.solveUsingRecursion(n - 1, nums)
         # return self.solveUsingMemoization(n - 1, nums, dp)
-        return self.solveUsingTabulation(nums, dp)
+        # return self.solveUsingTabulation(nums, dp)
+        return self.solveUsingSpaceOptimization(nums, dp)
 
     # TC: O(2 ^ n) (exponential)
     # SC: O(n) (recursion stack space)
@@ -71,6 +72,39 @@ class Solution:
             dp[ind] = max(pick, notPick)
         
         return dp[n - 1]
+
+
+    # TC: O(n)
+    # SC: O(1)
+    def solveUsingSpaceOptimization(self, nums, dp):
+        n = len(nums)
+
+        prev = nums[0]
+        prev2 = 0
+        curr = 0
+
+        for ind in range(1, n):
+            
+            # Pick (If we pick this element, then we will skip its next element as to avoid adjacents)
+            pick = nums[ind]
+            if ind > 1:
+                pick += prev2
+
+            # Not Pick
+            notPick = 0 + prev
+
+            curr = max(pick, notPick)
+
+            # Set the variables for next iteration
+            prev2 = prev
+            prev = curr
+
+        return prev # Note: Can also return curr here
+
+            
+
+
+
 
         
 
