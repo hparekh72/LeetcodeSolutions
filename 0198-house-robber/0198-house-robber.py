e19ddf2b36e7d675f721e2e7f606f217de90b967
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-09-01 14:09:46 -0700

    Time: 34 ms (71.37%), Space: 16.6 MB (27.8%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,7 +4,8 @@ class Solution:
         dp = [-1 for _ in range(n)]
 
         # return self.solveUsingRecursion(n - 1, nums)
-        return self.solveUsingMemoization(n - 1, nums, dp)
+        # return self.solveUsingMemoization(n - 1, nums, dp)
+        return self.solveUsingTabulation(nums, dp)
 
     # TC: O(2 ^ n) (exponential)
     # SC: O(n) (recursion stack space)
@@ -47,6 +48,32 @@ class Solution:
 
         dp[ind] = max(pick, notPick)
         return dp[ind]
+
+
+    # TC: O(n)
+    # SC: O(n) (dp array)
+
+    def solveUsingTabulation(self, nums, dp):
+        n = len(nums)
+
+        # Base Case
+        dp[0] = nums[0]
+
+        for ind in range(1, n):
+            # Pick (If we pick this element, then we will skip its next element as to avoid adjacents)
+            pick = nums[ind] 
+            if ind > 1:
+                pick += dp[ind - 2]
+
+            # Not Pick
+            notPick = 0 + dp[ind - 1]
+
+            dp[ind] = max(pick, notPick)
+        
+        return dp[n - 1]
+
+        
+
 
 
         
