2120cdbc07ac5c2c66bc1425c7cc4534e02d865e
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-09-01 00:18:02 -0700

    Time: 27 ms (93.02%), Space: 16.4 MB (90.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,7 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = [-1 for _ in range(n + 1)]
-        return self.solveUsingMemoization(n, dp)
+        # return self.solveUsingMemoization(n, dp)
+        # return self.solveUsingTabulation(n, dp)
+        return self.solveUsingSpaceOptimization(n)
+
 
     
     # TC: O(n) 
@@ -15,6 +18,32 @@ class Solution:
 
         dp[n] = self.solveUsingMemoization(n - 1, dp) + self.solveUsingMemoization(n - 2, dp)
         return dp[n]
+
+    # TC: O(n) 
+    # SC: O(n) (dp array)
+    def solveUsingTabulation(self, n, dp):
+        dp[0], dp[1] = 1, 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
+
+    def solveUsingSpaceOptimization(self, n):
+        prev2 = 1
+        prev = 1
+        
+        curr = prev
+        for i in range(2, n + 1):
+            curr = prev + prev2
+            prev2 = prev
+            prev = curr
+
+        return curr
+            
+
+
+
 
 
 
