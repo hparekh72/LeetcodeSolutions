c2e38067967bbaf59a23b00315cb0228c6e596e6
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-09-04 13:05:02 -0700

    Time: 38 ms (38.81%), Space: 16.6 MB (40.39%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -4,8 +4,9 @@ class Solution:
 
         dp = [[-1 for _ in range(n)] for _ in range(m)]
         
-        return self.solveUsingMemoization(0, 0, m, n, dp)
-
+        # return self.solveUsingMemoization(0, 0, m, n, dp)
+        # return self.solveUsingMemoization2(m - 1, n - 1, m, n, dp)
+        return self.solveUsingTabulation(m, n, dp)
 
     # TC: 2^(m * n)
     # SC: O(m + n) (recussion stack space)
@@ -24,6 +25,8 @@ class Solution:
 
         return down + right
     
+
+    # Memoization Approach 1
     # TC: O(m * n)
     # SC: O(m * n) (dp array) + O(m + n) (recursion stack space)
     def solveUsingMemoization(self, r, c, m, n, dp):
@@ -43,3 +46,56 @@ class Solution:
 
         dp[r][c] = down + right
         return dp[r][c]
+
+
+    # Memoization Approach 2
+    # TC: O(m * n)
+    # SC: O(m * n) (dp array) + O(m + n) (recursion stack space)
+
+    def solveUsingMemoization2(self, r, c, m, n, dp):
+        # Base Case
+        if r < 0 or c < 0: # Boundary condition
+            return 0
+
+        if r == 0 and c == 0:
+            return 1
+
+        if dp[r][c] != -1:
+            return dp[r][c]
+
+        up = self.solveUsingMemoization2(r - 1, c, m, n, dp)
+        left = self.solveUsingMemoization2(r, c - 1, m, n, dp)
+
+        dp[r][c] = up + left
+        
+        return dp[r][c]
+
+    # Tabulation
+    # TC: O(m * n)
+    # SC: O(m * n) (dp array) 
+
+    def solveUsingTabulation(self, m, n, dp):
+        for r in range(m):
+            for c in range(n):
+                if r == 0 and c == 0:
+                    dp[r][c] = 1
+                else:
+                    up, left = 0, 0
+                    if r > 0:
+                        up = dp[r - 1][c]
+                    
+                    if c > 0:
+                        left = dp[r][c - 1]
+
+                    dp[r][c] = up + left
+
+        return dp[m - 1][n - 1]
+
+
+
+    
+
+
+
+
+
