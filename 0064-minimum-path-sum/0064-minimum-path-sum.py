b3584b806b34021d7c8606513b2a58d350d995e8
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-09-05 11:53:25 -0700

    Time: 145 ms (5.32%), Space: 17.6 MB (91.82%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -7,7 +7,9 @@ class Solution:
 
         dp = [[-1 for _ in range(n)] for _ in range(m)]
 
-        return self.solveUsingMemoization(m - 1, n - 1, m , n, grid, dp)
+        # return self.solveUsingMemoization(m - 1, n - 1, m , n, grid, dp)
+        # return self.solveUsingTabulation(m , n, grid, dp)
+        return self.solveUsingSpaceOptimization(m, n, grid)
 
 
     # TC: O(2^(m * n))
@@ -44,6 +46,54 @@ class Solution:
 
         dp[r][c] = min(left, up)
         return dp[r][c]
+
+
+    # TC: O(m * n)
+    # SC: O(m * n) (dp array) 
+
+    def solveUsingTabulation(self, m, n, grid, dp):
+
+        for r in range(m):
+            for c in range(n):
+                if r == 0 and c == 0:
+                    dp[r][c] = grid[r][c]
+                else:
+                    left, up = float('inf'), float('inf')
+                    if c > 0:
+                        left = grid[r][c] + dp[r][c - 1]
+                    if r > 0:
+                        up = grid[r][c] + dp[r - 1][c]
+
+                    dp[r][c] = min(left, up)
+
+        return dp[m - 1][n - 1]
+
+
+    # TC: O(m * n)
+    # SC: O(n) (prevRow and currRow array) 
+
+    def solveUsingSpaceOptimization(self, m, n, grid):
+        prevRow = [-1] * n
+        for r in range(m):
+            currRow = [-1] * n
+            for c in range(n):
+                if r == 0 and c == 0:
+                    currRow[c] = grid[r][c]
+                else:
+                    left, up = float('inf'), float('inf')
+                    if c > 0:
+                        left = grid[r][c] + currRow[c - 1]
+                    if r > 0:
+                        up = grid[r][c] + prevRow[c]
+
+                    currRow[c] = min(left, up)
+            prevRow = currRow
+
+        return prevRow[n - 1]
+
+
+
+                
 
 
 
