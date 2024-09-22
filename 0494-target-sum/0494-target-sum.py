f244f9062cfa185435263b45fb716e3b3922f74e
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-09-21 17:16:03 -0700

    Time: 89 ms (87.93%), Space: 17 MB (68.16%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -3,8 +3,12 @@ class Solution:
         n = len(nums)
         # return self.solveUsingRecursion(n - 1, target, nums)
 
-        dp = defaultdict(int)
-        return self.solveUsingMemoization(n - 1, target, nums, dp)
+        # dp = defaultdict(int)
+        # return self.solveUsingMemoization(n - 1, target, nums, dp)
+
+        return self.countPartitions(n, target, nums)
+
+
 
     # TC: O(2^n)
     # SC: O(n) (recursion stack space)
@@ -46,6 +50,124 @@ class Solution:
 
         dp[(ind, target)] = add + subtract
         return dp[(ind, target)]
+
+    # Approach 2: Divide them into two subsets such that the difference betweem those subsets are target
+    def countPartitions(self, n, d, arr):
+        # code here
+        
+        # s1 + s2 = totalSum
+        # s1 - s2 = d 
+        # totalSum - s2 - s2 = d
+        # -2s2 + totalSum = d
+        # (totalSum - d) // 2 = s2
+        
+        totalSum = sum(arr)
+        
+        if (totalSum - d) < 0 or (totalSum - d) % 2 == 1:
+            return 0
+        
+        target = (totalSum - d) // 2
+        
+        dp = [[-1 for _ in range(target + 1)] for _ in range(n)]
+        
+        # return self.solveUsingMemoization(n - 1, target, arr, dp)
+        
+        return self.solveUsingTabulation(target, arr, n)
+        
+        # return self.solveUsingSpaceOptimization(target, arr, n)
+
+    # def solveUsingMemoization(self, ind, target, arr, dp):
+    #     mod = 10**9 + 7
+        
+    #     # Base Case
+    #     if ind == 0:
+    #         if arr[0] == 0 and target == 0:
+    #             return 2
+    #         if arr[0] == target or target == 0:
+    #             return 1
+    #         return 0
+            
+            
+    #     if dp[ind][target] != -1:
+    #         return dp[ind][target]
+        
+    #     # Not Pick 
+    #     notPick = self.solveUsingMemoization(ind - 1, target, arr, dp)
+        
+    #     # Pick
+    #     pick = 0
+    #     if arr[ind] <= target:
+    #         pick = self.solveUsingMemoization(ind - 1, target - arr[ind], arr, dp)
+            
+    #     dp[ind][target] = (pick + notPick) % mod
+        
+    #     return dp[ind][target] 
+    
+    
+    # TC: O(n * k) 
+    # SC: O(n * k)
+    def solveUsingTabulation(self, target, arr, n):
+        mod = 10**9 + 7
+        
+        dp = [[0 for _ in range(target + 1)] for _ in range(n)]
+        
+        if arr[0] == 0:
+            dp[0][0] = 2
+        else:
+            dp[0][0] = 1
+            
+        if arr[0] != 0 and arr[0] <= target:
+            dp[0][arr[0]] = 1
+            
+        for ind in range(1, n):
+            for t in range(target + 1):
+                
+                # Not Pick 
+                notPick = dp[ind - 1][t] 
+                
+                # Pick
+                pick = 0
+                if arr[ind] <= t:
+                    pick = dp[ind - 1][t - arr[ind]] 
+                    
+                dp[ind][t] = (pick + notPick) % mod
+                
+        return dp[n - 1][target]
+
+
+    # TC: O(n * k) 
+    # SC: O(k) 
+    def solveUsingSpaceOptimization(self, target, arr, n):
+        mod = 10**9 + 7
+        
+        prev = [0] * (target + 1)
+        
+        if arr[0] == 0:
+            prev[0] = 2
+        else:
+            prev[0] = 1
+            
+        if arr[0] != 0 and arr[0] <= target:
+            prev[arr[0]] = 1
+            
+        for ind in range(1, n):
+            curr = [0] * (target + 1)
+            for t in range(target + 1):
+                # Not Pick 
+                notPick = prev[t] 
+                
+                # Pick
+                pick = 0
+                if arr[ind] <= t:
+                    pick = prev[t - arr[ind]] 
+                    
+                curr[t] = (pick + notPick) % mod
+            
+            prev = [num for num in curr]
+                
+        return prev[target]
+
+
 
 
 
