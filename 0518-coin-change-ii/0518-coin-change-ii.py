b6f6ffd1ccafeff67ed604379e53b8bc44e30476
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-09-21 18:07:38 -0700

    Time: 370 ms (32.19%), Space: 29.2 MB (48.89%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -2,8 +2,16 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
         # return self.solveUsingRecursion(n - 1, amount, coins)
+
         dp = [[-1 for _ in range(amount + 1)] for _ in range(n)]
-        return self.solveUsingMemoization(n - 1, amount, coins, dp)
+        # return self.solveUsingMemoization(n - 1, amount, coins, dp)
+
+        # return self.solveUsingTabulation(amount, coins, n)
+        return self.solveUsingSpaceOptimization(amount, coins, n)
+
+
+    # TC: O(2^n)
+    # SC: O(n) (recursion stack space)
 
     def solveUsingRecursion(self, ind, amount, coins):
         # Base Case
@@ -22,6 +30,9 @@ class Solution:
             pick = self.solveUsingRecursion(ind, amount - coins[ind], coins)
 
         return pick + notPick
+
+    # TC: O(n * amount)
+    # SC: O(n * amount) + O(amount) (recursion stack space)
 
     def solveUsingMemoization(self, ind, amount, coins, dp):
         # Base Case
@@ -44,6 +55,65 @@ class Solution:
 
         dp[ind][amount] = pick + notPick
         return dp[ind][amount]
+
+
+    # TC: O(n * amount)
+    # SC: O(n * amount) 
+
+    def solveUsingTabulation(self, amount, coins, n):
+        dp = [[0 for _ in range(amount + 1)] for _ in range(n)]
+
+        # Base Case
+        for num in range(amount + 1):
+            if num % coins[0] == 0:
+                dp[0][num] = 1
+        
+        for ind in range(1, n):
+            for num in range(amount + 1):
+                # Not Pick
+                notPick = dp[ind - 1][num] 
+
+                # Pick
+                pick = 0
+                if coins[ind] <= num:
+                    pick = dp[ind][num - coins[ind]] 
+
+                dp[ind][num] = pick + notPick
+        
+        return dp[n - 1][amount]
+
+
+    # TC: O(n * amount)
+    # SC: O(amount) 
+
+    def solveUsingSpaceOptimization(self, amount, coins, n):
+        prev = [0 for _ in range(amount + 1)] 
+        
+        # Base Case
+        for num in range(amount + 1):
+            if num % coins[0] == 0:
+                prev[num] = 1
+        
+        for ind in range(1, n):
+            curr = [0 for _ in range(amount + 1)] 
+            for num in range(amount + 1):
+                # Not Pick
+                notPick = prev[num] 
+
+                # Pick
+                pick = 0
+                if coins[ind] <= num:
+                    pick = curr[num - coins[ind]] 
+
+                curr[num] = pick + notPick
+
+            prev = curr
+
+        return prev[amount]
+
+         
+
+
 
     
         
