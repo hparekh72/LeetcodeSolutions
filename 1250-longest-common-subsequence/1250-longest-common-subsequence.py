f4ec6bfbda9ff30d3e432c4d35dbd5482462ab75
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-09-23 21:08:49 -0700

    Time: 711 ms (25.84%), Space: 27.5 MB (85.56%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -0,0 +1,69 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        l1 = len(text1)
+        l2 = len(text2)
+        # return self.solveUsingRecursion(l1 -1, l2 - 1, text1, text2) # 0 based indexing
+        # return self.solveUsingRecursion1(l1, l2, text1, text2) # 1 based indexing
+        
+        dp = [[-1 for _ in range(l2 + 1)] for _ in range(l1 + 1)]
+        return self.solveUsingMemoization(l1, l2, text1, text2, dp) # 1 based indexing
+
+
+
+    # TC: O(2^l1 + 2^l2)
+    # SC: O(l1 + l2)
+    def solveUsingRecursion(self, ind1, ind2, text1, text2):
+        # Base Case
+        if ind1 < 0 or ind2 < 0:
+            return 0
+
+        # Matching
+        if text1[ind1] == text2[ind2]:
+            return 1 + self.solveUsingRecursion(ind1 - 1, ind2 - 1, text1, text2)
+
+        # Not Matching
+        return 0 + max(self.solveUsingRecursion(ind1 - 1, ind2, text1, text2), self.solveUsingRecursion(ind1, ind2 - 1, text1, text2))
+
+    # TC: O(2^l1 + 2^l2)
+    # SC: O(l1 + l2)
+    def solveUsingRecursion1(self, ind1, ind2, text1, text2):
+        # Base Case
+        if ind1 == 0 or ind2 == 0:
+            return 0
+
+        # Matching
+        if text1[ind1 - 1] == text2[ind2 - 1]:
+            return 1 + self.solveUsingRecursion1(ind1 - 1, ind2 - 1, text1, text2)
+
+        # Not Matching
+        return 0 + max(self.solveUsingRecursion1(ind1 - 1, ind2, text1, text2), self.solveUsingRecursion1(ind1, ind2 - 1, text1, text2))
+
+
+
+    # TC: O(l1 * l2)
+    # SC: O(l1 * l2) + O(l1 + l2) (recursion stack space)
+    def solveUsingMemoization(self, ind1, ind2, text1, text2, dp):
+        # Base Case
+        if ind1 == 0 or ind2 == 0:
+            return 0
+
+        if dp[ind1][ind2] != -1:
+            return dp[ind1][ind2]
+
+        # Matching 
+        if text1[ind1 - 1] == text2[ind2 - 1]:
+            dp[ind1][ind2] = 1 + self.solveUsingMemoization(ind1 - 1, ind2 - 1, text1, text2, dp)
+        else:
+            # Not Matching
+            dp[ind1][ind2] = 0 + max(self.solveUsingMemoization(ind1 - 1, ind2, text1, text2, dp), self.solveUsingMemoization(ind1, ind2 - 1, text1, text2, dp))
+
+        return dp[ind1][ind2]
+    
+
+
+
+
+        
+
+
+        
