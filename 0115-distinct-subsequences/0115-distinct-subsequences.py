dc88046a24457e45ee3f1d18aec544a7064b915e
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-10-02 02:01:22 -0700

    Time: 416 ms (37.48%), Space: 25.8 MB (79.11%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,75 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        l1 = len(s)
+        l2 = len(t)
+        # return self.solveUsingRecursion(l1 - 1, l2 - 1, s, t) # 0-based indexing
+
+        # return self.solveUsingRecursion1(l1, l2, s, t) # 1-based indexing
+
+        dp = [[-1 for _ in range(l2 + 1)] for _ in range(l1 + 1)]
+
+        return self.solveUsingMemoization(l1, l2, s, t, dp) # 1-based indexing
+
+
+
+    # TC: O(2^l1 * 2^l2) (exponential)
+    # SC: O(l1 + l2) (recursion stack space)
+    def solveUsingRecursion(self, ind1, ind2, s, t): # 0-based indexing
+        # Base Case
+
+        if ind2 < 0:
+            return 1
+
+        if ind1 < 0:
+            return 0
+
+        # If Matching Characters, 2 choices (Pick or Not pick)
+        if s[ind1] == t[ind2]:
+            return self.solveUsingRecursion(ind1 - 1, ind2 - 1, s, t) + self.solveUsingRecursion(ind1 - 1, ind2, s, t)  
+
+        # Not Matching Characters, cannot pick, move ind1
+        return self.solveUsingRecursion(ind1 - 1, ind2, s, t) 
+
+    # TC: O(2^l1 * 2^l2) (exponential)
+    # SC: O(l1 + l2) (recursion stack space)
+    def solveUsingRecursion1(self, ind1, ind2, s, t): # 1-based indexing
+        # Base Case
+
+        if ind2 == 0:
+            return 1
+
+        if ind1 == 0:
+            return 0
+
+        # If Matching Characters, 2 choices (Pick or Not pick)
+        if s[ind1 - 1] == t[ind2 - 1]: # 1-based indexing
+            return self.solveUsingRecursion1(ind1 - 1, ind2 - 1, s, t) + self.solveUsingRecursion1(ind1 - 1, ind2, s, t)  
+
+        # Not Matching Characters, cannot pick, move ind1
+        return self.solveUsingRecursion1(ind1 - 1, ind2, s, t) 
+
+    # TC: O(l1 * l2) 
+    # SC: O(l1 * l2) + O(l1 + l2) (recursion stack space)
+    def solveUsingMemoization(self, ind1, ind2, s, t, dp): # 1-based indexing
+        # Base Case
+
+        if ind2 == 0:
+            return 1
+
+        if ind1 == 0:
+            return 0
+
+        if dp[ind1][ind2] != -1:
+            return dp[ind1][ind2]
+
+        # If Matching Characters, 2 choices (Pick or Not pick)
+        if s[ind1 - 1] == t[ind2 - 1]: # 1-based indexing
+            dp[ind1][ind2] = self.solveUsingMemoization(ind1 - 1, ind2 - 1, s, t, dp) + self.solveUsingMemoization(ind1 - 1, ind2, s, t, dp)     
+        else:
+            # Not Matching Characters, cannot pick, move ind1
+            dp[ind1][ind2] = self.solveUsingMemoization(ind1 - 1, ind2, s, t, dp) 
+
+        return dp[ind1][ind2]
+
+
+
