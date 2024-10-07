81b13fac61c0eab32437e0508db198b4072e56fd
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-10-06 21:16:56 -0700

    Time: 286 ms (75.49%), Space: 25.8 MB (33.97%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -0,0 +1,93 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        l1 = len(s)
+        l2 = len(p)
+
+        # return self.solveUsingRecursion(l1 - 1, l2 - 1, s, p) # 0-based indexing
+        # return self.solveUsingRecursion1(l1, l2, s, p) # 1-based indexing
+
+        dp = [[-1 for _ in range(l2 + 1)] for _ in range(l1 + 1)]
+        return self.solveUsingMemoization(l1, l2, s, p, dp) # 1-based indexing
+
+
+    # TC: O(Exponentil)
+    # SC: O(l1 + l2) (recursion stack space)
+    def solveUsingRecursion(self, ind1, ind2, s, p): # 0-based indexing
+        # Base Case
+        if ind1 < 0 and ind2 < 0:
+            return True
+        elif ind1 >= 0 and ind2 < 0:
+            return False
+        elif ind1 < 0 and ind2 >= 0:
+            for i in range(ind2+1):
+                if p[i] != "*":
+                    return False
+            return True
+
+        # Matching Character or ?
+        if s[ind1] == p[ind2] or p[ind2] == '?':
+            return self.solveUsingRecursion(ind1 - 1, ind2 - 1, s, p)
+        elif p[ind2] == "*": # For *
+            # When * is consider as empty "" or when * is considered as one or more character 
+            return  self.solveUsingRecursion(ind1, ind2 - 1, s, p) or self.solveUsingRecursion(ind1 - 1, ind2, s, p)
+        else: # When characters does not match
+            return False
+
+
+    # TC: O(Exponentil)
+    # SC: O(l1 + l2) (recursion stack space)
+    def solveUsingRecursion1(self, ind1, ind2, s, p): # 1-based indexing
+        # Base Case
+        if ind1 == 0 and ind2 == 0:
+            return True
+        elif ind1 > 0 and ind2 == 0:
+            return False
+        elif ind1 == 0 and ind2 > 0:
+            for i in range(1, ind2 + 1):
+                if p[i - 1] != "*":
+                    return False
+            return True
+
+        # Matching Character or ?
+        if s[ind1 - 1] == p[ind2 - 1] or p[ind2 - 1] == '?':
+            return self.solveUsingRecursion1(ind1 - 1, ind2 - 1, s, p)
+        elif p[ind2 - 1] == "*": # For *
+            # When * is consider as empty "" or when * is considered as one or more character 
+            return  self.solveUsingRecursion1(ind1, ind2 - 1, s, p) or self.solveUsingRecursion1(ind1 - 1, ind2, s, p)
+        else: # When characters does not match
+            return False
+
+
+    # TC: O(l1 * l2)
+    # SC: O(l1 * l2) + O(l1 + l2) (recursion stack space)
+    def solveUsingMemoization(self, ind1, ind2, s, p, dp):
+        # Base Case
+        if ind1 == 0 and ind2 == 0:
+            return True
+        elif ind1 > 0 and ind2 == 0:
+            return False
+        elif ind1 == 0 and ind2 > 0:
+            for i in range(1, ind2 + 1):
+                if p[i - 1] != "*":
+                    return False
+            return True
+
+        if dp[ind1][ind2] != -1:
+            return dp[ind1][ind2]
+        
+        # Matching Character or ?
+        if s[ind1 - 1] == p[ind2 - 1] or p[ind2 - 1] == '?':
+            dp[ind1][ind2] = self.solveUsingMemoization(ind1 - 1, ind2 - 1, s, p, dp)
+        elif p[ind2 - 1] == "*": # For *
+            # When * is consider as empty "" or when * is considered as one or more character 
+            dp[ind1][ind2] = self.solveUsingMemoization(ind1, ind2 - 1, s, p, dp) or self.solveUsingMemoization(ind1 - 1, ind2, s, p, dp)
+        else: # When characters does not match
+            dp[ind1][ind2] = False
+
+        return dp[ind1][ind2]
+
+        
+        
+
+
+        
