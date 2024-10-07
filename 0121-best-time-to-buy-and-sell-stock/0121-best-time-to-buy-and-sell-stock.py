92feb6f685703a4ea823d043c7882a144edbea6c
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-10-06 22:18:32 -0700

    Time: 810 ms (9.12%), Space: 27.5 MB (13.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -13,16 +13,31 @@ class Solution:
 
     # Optimal
     # TC: O(n)
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     n = len(prices) # days
+    #     maxProfit = 0       
+    #     buyPrice = prices[0]
+    #     for i in range(1, n):
+    #         if prices[i] <= buyPrice:
+    #             buyPrice = prices[i]
+    #         else:
+    #             sellPrice = prices[i]
+    #             maxProfit = max(maxProfit, sellPrice - buyPrice)
+
+    #     return maxProfit
+
+
+    # Note: Why this is a DP problem ? Reason: As we are remembering the past(minPrice)
+    # Optimal
+    # TC: O(n)
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices) # days
-        maxProfit = 0       
-        buyPrice = prices[0]
+        n = len(prices)
+        minPrice, maxProfit = prices[0], 0
+
         for i in range(1, n):
-            if prices[i] <= buyPrice:
-                buyPrice = prices[i]
-            else:
-                sellPrice = prices[i]
-                maxProfit = max(maxProfit, sellPrice - buyPrice)
+            minPrice = min(minPrice, prices[i])
+            maxProfit = max(maxProfit, prices[i] - minPrice)
 
         return maxProfit
+
         
