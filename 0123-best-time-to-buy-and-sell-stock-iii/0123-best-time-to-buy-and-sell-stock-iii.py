53a796c903144be204dc159f09501abf398b6b21
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-10-07 20:08:05 -0700

    Time: 1860 ms (19.19%), Space: 64.6 MB (18.93%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -2,9 +2,11 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # return self.solveUsingRecursion(0, 1, 2, prices)
 
-        n = len(prices)
-        dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n)]
-        return self.solveUsingMemoization(0, 1, 2, prices, dp)
+        # n = len(prices)
+        # dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n)]
+        # return self.solveUsingMemoization(0, 1, 2, prices, dp)
+
+        return self.solveUsingTabulation(prices)
 
 
     # Buy = 1 -> Can Buy,
@@ -58,6 +60,33 @@ class Solution:
 
         dp[ind][buy][capacity] = profit
         return dp[ind][buy][capacity]
+
+
+    # TC: O(n * 2 * 3)
+    # SC: O(n * 2 * 3) 
+    def solveUsingTabulation(self, prices):
+        n = len(prices)
+        capacity = 2
+        dp = dp = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(n + 1)]
+
+        # The base case is already covered as the DP array is initialized to 0
+
+        for ind in range(n - 1, -1, -1):
+            for buy in range(2):
+                for capacity in range(1, 3):
+                    profit = 0
+                    if buy:
+                        # Buy: (2 choices: Can buy, Cannot buy)
+                        profit = max(-prices[ind] + dp[ind + 1][0][capacity], 0 + dp[ind + 1][1][capacity])
+                    else:
+                        # Sell: (2 choices: Can sell, Cannot sell)
+                        profit = max(prices[ind] + dp[ind + 1][1][capacity - 1], 0 + dp[ind + 1][0][capacity])
+
+                    dp[ind][buy][capacity] = profit
+        return dp[0][1][2]
+        
+
+
 
 
 
