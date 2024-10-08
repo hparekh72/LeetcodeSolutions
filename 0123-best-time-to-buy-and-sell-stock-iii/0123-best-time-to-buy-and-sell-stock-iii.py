bbe9c8db853e7f01262448e0c59bd5fd29b877d8
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-10-07 19:57:32 -0700

    Time: 2071 ms (14.45%), Space: 89.1 MB (13.49%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,67 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # return self.solveUsingRecursion(0, 1, 2, prices)
+
+        n = len(prices)
+        dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n)]
+        return self.solveUsingMemoization(0, 1, 2, prices, dp)
+
+
+    # Buy = 1 -> Can Buy,
+    # Buy = 0 -> Cannot Buy
+
+
+    # TC: O(2^n)
+    # SC: O(n) (recursion stack space)
+    def solveUsingRecursion(self, ind, buy, capacity, prices):
+        # Base Case
+        if ind == len(prices) or capacity == 0:
+            return 0
+
+        profit = 0
+        if buy:
+            # Buy: (2 choices: Can buy, Cannot buy)
+            profit = max(
+                -prices[ind] + self.solveUsingRecursion(ind + 1, 0, capacity, prices), 
+            0 + self.solveUsingRecursion(ind + 1, 1, capacity, prices))
+        else:
+            # Sell: (2 choices: Can sell, Cannot sell)
+            profit = max(
+                prices[ind] + self.solveUsingRecursion(ind + 1, 1, capacity - 1, prices), 
+                0 + self.solveUsingRecursion(ind + 1, 0, capacity, prices))
+
+        return profit
+
+
+    # TC: O(n * 2 * 3)
+    # SC: O(n * 2 * 3) + O(n) (recursion stack space)
+
+    def solveUsingMemoization(self, ind, buy, capacity, prices, dp):
+        # Base Case
+        if ind == len(prices) or capacity == 0:
+            return 0
+
+        if dp[ind][buy][capacity] != -1:
+            return dp[ind][buy][capacity]
+
+        profit = 0
+        if buy:
+            # Buy: (2 choices: Can buy, Cannot buy)
+            profit = max(
+                -prices[ind] + self.solveUsingMemoization(ind + 1, 0, capacity, prices, dp), 
+            0 + self.solveUsingMemoization(ind + 1, 1, capacity, prices, dp))
+        else:
+            # Sell: (2 choices: Can sell, Cannot sell)
+            profit = max(
+                prices[ind] + self.solveUsingMemoization(ind + 1, 1, capacity - 1, prices, dp), 
+                0 + self.solveUsingMemoization(ind + 1, 0, capacity, prices, dp))
+
+        dp[ind][buy][capacity] = profit
+        return dp[ind][buy][capacity]
+
+
+
+
+
+
+        
