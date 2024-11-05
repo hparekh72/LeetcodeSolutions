3198662852b81957fba111663e15505ebfe1a743
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2024-11-04 19:12:20 -0800

    Time: 362 ms (24.28%), Space: 27.8 MB (25.03%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,9 +2,11 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         # return self.solveUsingRecursion(0, 1, prices, fee)
 
-        n = len(prices)
-        dp = [[-1 for _ in range(2)] for _ in range(n)]
-        return self.solveUsingMemoization(0, 1, prices, fee, dp)
+        # n = len(prices)
+        # dp = [[-1 for _ in range(2)] for _ in range(n)]
+        # return self.solveUsingMemoization(0, 1, prices, fee, dp)
+
+        return self.solveUsingTabulation(prices, fee)
 
     # Buy -> 1
     # Cannot Buy -> 0
@@ -48,5 +50,31 @@ class Solution:
 
         dp[ind][buy] = profit 
         return dp[ind][buy]
+
+
+
+    # TC: O(n * 2)
+    # SC: O(n * 2) 
+    def solveUsingTabulation(self, prices, fee):
+        n = len(prices)
+        dp = [[-1 for _ in range(2)] for _ in range(n + 1)]
+
+        # Base Case
+        dp[n][0] = dp[n][1] = 0
+
+        for ind in range(n - 1, -1, -1):
+            for buy in range(2):
+                profit = 0 
+                if buy:
+                    # Buy: 2 choices (Can Buy, Cannot Buy)
+                    profit = max(-prices[ind] + dp[ind + 1][0],  dp[ind + 1][1])
+                else:
+                    # Sell: 2 choices (Can Sell, Cannot Sell)
+                    profit = max(prices[ind] - fee + dp[ind + 1][1], dp[ind + 1][0])
+                
+                dp[ind][buy] = profit 
+
+        return dp[0][1] 
+
 
         
