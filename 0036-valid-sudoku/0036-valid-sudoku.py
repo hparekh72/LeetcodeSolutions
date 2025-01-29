3b155694a5e9be1480730e544f86ce89e66c5cf3
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-01-29 19:55:51 +0530

    Time: 9 ms (14.61%), Space: 17.9 MB (33.69%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,57 +1,97 @@
 from collections import defaultdict
 
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        def checkValidity(x, y, board):  # Checks the x-axis 
-            hashSet = set()
-            for i in range(len(board[0])):
-                if board[x][i] == ".":
-                    continue
-                
-                if board[x][i] not in hashSet:
-                    hashSet.add(board[x][i])
-                else:
-                    return False
-
-
-            hashSet = set()
-            for i in range(len(board)): # Checks the y-axis 
-                if board[i][y] == ".":
-                    continue
-                
-                if board[i][y] not in hashSet:
-                    hashSet.add(board[i][y])
-                else:
-                    return False
-
-
-            m = (x // 3) * 3 
-            n = (y // 3) * 3
-            hashSet = set()
-            for i in range(m, m+3): # Checks the sub-box
-                for j in range(n, n+3):
-                    if board[i][j] == ".":
-                        continue
-                
-                    if board[i][j] not in hashSet:
-                        hashSet.add(board[i][j])
-                    else:
-                        return False
-
-            return True
-
-
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == ".":
-                    continue
-
-                if checkValidity(i, j, board) == False:
-                    return False
-        return True
-
     # def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        # def checkValidity(x, y, board):  # Checks the x-axis 
+        #     hashSet = set()
+        #     for i in range(len(board[0])):
+        #         if board[x][i] == ".":
+        #             continue
+                
+        #         if board[x][i] not in hashSet:
+        #             hashSet.add(board[x][i])
+        #         else:
+        #             return False
+
+
+        #     hashSet = set()
+        #     for i in range(len(board)): # Checks the y-axis 
+        #         if board[i][y] == ".":
+        #             continue
+                
+        #         if board[i][y] not in hashSet:
+        #             hashSet.add(board[i][y])
+        #         else:
+        #             return False
+
+
+        #     m = (x // 3) * 3 
+        #     n = (y // 3) * 3
+        #     hashSet = set()
+        #     for i in range(m, m+3): # Checks the sub-box
+        #         for j in range(n, n+3):
+        #             if board[i][j] == ".":
+        #                 continue
+                
+        #             if board[i][j] not in hashSet:
+        #                 hashSet.add(board[i][j])
+        #             else:
+        #                 return False
+
+        #     return True
+
+
+        # for i in range(len(board)):
+        #     for j in range(len(board[0])):
+        #         if board[i][j] == ".":
+        #             continue
+
+        #         if checkValidity(i, j, board) == False:
+        #             return False
+        # return True
+
+        # ------------------x------------------------x-----------------------x-----------------
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        TC: O(n^2)
+        SC: O(n^2)
+        for row in range(9): # Checking Rows
+            seen = set()
+            for i in range(9):
+                if board[row][i] == ".":
+                    continue
+                if board[row][i] in seen:
+                    return False
+                seen.add(board[row][i])
+
+        for col in range(9): # Checking Columns
+            seen = set()
+            for i in range(9):
+                if board[i][col] == ".":
+                    continue
+                if board[i][col] in seen:
+                    return False
+                
+                seen.add(board[i][col])
+            
+        for square in range(9): # Checking Squares
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (square // 3) * 3 + i # Start Row: (square // 3) * 3
+                    col = (square % 3) * 3 + j  # Start Col: (square % 3) * 3
+
+                    if board[row][col] == ".":
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+        return True
+                
+
+    # def isValidSudoku(self, board: List[List[str]]) -> bool: # One Pass
+    #     # TC: O(n^2)
+    #     # SC: O(n^2)
     #     rows = defaultdict(set)
     #     cols = defaultdict(set)
     #     squares = defaultdict(set)
