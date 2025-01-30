3476db72ed81af62671edf465b5e633480373463
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-01-30 19:25:24 +0530

    Time: 19 ms (43.17%), Space: 19.3 MB (31.92%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -11,33 +11,52 @@ class Solution:
         # Store the maxLeft and maxRight for each index in seperate arrays
         # TC: O(N)
         # SC: O(N)
-        n = len(height)
-        if n == 0:
-            return 0
 
-        leftMax = [0] * n
-        rightMax = [0] * n
+        # n = len(height)
+        # if n == 0:
+        #     return 0
 
-        leftMax[0] = height[0]
-        for i in range(1, n):
-            leftMax[i] = max(leftMax[i-1], height[i])
+        # leftMax = [0] * n
+        # rightMax = [0] * n
 
-        rightMax[n - 1] = height[n - 1]
-        for i in range(n - 2, -1, -1):
-            rightMax[i] = max(rightMax[i + 1], height[i])
+        # leftMax[0] = height[0]
+        # for i in range(1, n):
+        #     leftMax[i] = max(leftMax[i-1], height[i])
 
-        res = 0
-        for i in range(1, n):
-            res += min(leftMax[i], rightMax[i]) - height[i]
+        # rightMax[n - 1] = height[n - 1]
+        # for i in range(n - 2, -1, -1):
+        #     rightMax[i] = max(rightMax[i + 1], height[i])
 
-        return res
+        # res = 0
+        # for i in range(n):
+        #     res += min(leftMax[i], rightMax[i]) - height[i]
 
-        
-
-
+        # return res
 
         # Approach3: Two Pointer
         # TC: O(N)
         # SC: O(1)
+
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax = height[l]
+        rightMax = height[r]
+        res = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                res += max(leftMax - height[l], 0)
+                leftMax = max(leftMax, height[l])
+
+            else:
+                r -= 1
+                res += max(rightMax - height[r], 0)
+                rightMax = max(rightMax, height[r])
+
+        return res
+            
 
  
