85b206199c176e15b01ee00dab8262efaf000132
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-04 00:45:03 +0530

    Time: 33 ms (92.81%), Space: 18.6 MB (47.98%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -8,31 +8,29 @@ class Node:
 """
 
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        # Brute
-        # TC: O(N)
-        # SC: O(N)
+    # def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]': # Brute Force
+    #     # TC: O(2N) ~ O(N)
+    #     # SC: O(N)
+
+    #     oldToCopy = {None: None} # Edge case to handle temp.random == None
+
+    #     temp = head
+    #     while temp:
+    #         copy = Node(temp.val)
+    #         oldToCopy[temp] = copy
+    #         temp = temp.next
         
-        # oldToCopy = {None: None} # Edge case to handle temp.random == None
+    #     temp = head
+    #     while temp:
+    #         copy = oldToCopy[temp]
+    #         copy.next = oldToCopy[temp.next]
+    #         copy.random = oldToCopy[temp.random]
+    #         temp = temp.next
 
-        # temp = head
-        # while temp != None:
-        #     oldToCopy[temp] = Node(temp.val)
-        #     temp = temp.next
-            
-        # temp = head
-        # while temp != None:
-        #     copy = oldToCopy[temp]
-        #     copy.next = oldToCopy[temp.next]
-        #     copy.random = oldToCopy[temp.random]
+    #     return  oldToCopy[head]
 
-        #     temp = temp.next
-
-
-        # return oldToCopy[head]
-
-        # Optimal
-        # TC: O(3N)
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]': # Space Optimization (Optimal)
+        # TC: O(3N) ~ O(N)
         # O(1)
 
         # Step1: Insert copy nodes in between
@@ -43,11 +41,10 @@ class Solution:
         self.connectRandomPointers(head)
         return self.getDeepCopyList(head)
 
-
     def insertCopyNodesInBetween(self, head):
         temp = head
 
-        while temp != None:
+        while temp:
             copyNode = Node(temp.val)
             copyNode.next = temp.next
             temp.next = copyNode
@@ -55,24 +52,24 @@ class Solution:
 
     def connectRandomPointers(self, head):
         temp = head
-        while temp != None:
+        while temp:
             copyNode = temp.next
-            if temp.random != None:
+            if temp.random:
                 copyNode.random = temp.random.next
             else:
                 copyNode.random = None
 
-            temp = temp.next.next
+            temp = temp.next.next # Moving to the next original node
 
     def getDeepCopyList(self, head):
         temp = head
         dummyNode = Node(-1)
         res = dummyNode
 
-        while temp != None:
+        while temp:
             copyNode = temp.next
-
-            res.next = copyNode 
+            
+            res.next = copyNode
             res = res.next # Move res to next node
 
             # Disconnecting and going back to the initial state of the LL
@@ -83,14 +80,8 @@ class Solution:
 
 
         
+    
 
 
-
-
-
-
-
-        
-        
 
 
