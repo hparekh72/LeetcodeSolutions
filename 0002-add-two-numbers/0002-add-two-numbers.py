a7303688678e4c2568d78388837ff90f83237718
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-04 02:44:41 +0530

    Time: 0 ms (100%), Space: 17.9 MB (47.46%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -7,18 +7,18 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
         #TC: O(max(l1, l2))
-        #SC: O(max(l1, l2))
+        #SC: O(max(l1, l2)) (needed), otherwise O(1)
 
         dummyNode = ListNode()
         curr = dummyNode
         carry = 0
 
-        while (l1 != None or l2 != None) or carry:
+        while l1 or l2 or carry:
             total = carry
             if l1:
                 total += l1.val
                 l1 = l1.next
-            
+                
             if l2:
                 total += l2.val
                 l2 = l2.next
@@ -32,6 +32,7 @@ class Solution:
         return dummyNode.next
 
 
-        
+            
+
 
         
