8f96e22984e64d5b93490bd0b8867797664d6601
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-04 03:43:59 +0530

    Time: 20 ms (87.75%), Space: 30.1 MB (30.74%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,6 +1,5 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-
         # Brute Force: Sorting
         # TC: O(NlogN)
         # SC: O(1)
@@ -10,8 +9,22 @@ class Solution:
         # SC: O(N)
 
         # Optimal: Linked List Cycle, slow and fast pointers
+        # Floyd's Tortoise and Hare (Cycle Detection) algorithm
+
+        # Intuition: 
+
+        # Treating the Array as a Linked List:
+            # 1) Imagine each index as a node in a linked list.
+            # 2) The value at each index points to the next node.
+
+        # Why a Cycle Exists:
+            # 1) Since there's a duplicate number, it creates a cycle in the linked list representation.
+            # 2) The entrance to this cycle represents the duplicate number.
+
         # TC: O(N)
         # SC: O(1)
+
+        # First Phase: Detect if a cycle exists (slow and fast pointers meet).
 
         slow = nums[0]
         fast = nums[0]
@@ -22,10 +35,18 @@ class Solution:
 
             if slow == fast:
                 break
-
-        fast = nums[0]
-        while slow != fast:
+        
+        # Second Phase: Find the start of the cycle (the duplicate number).
+        slow2 = nums[0]
+        while slow != slow2:
             slow = nums[slow]
-            fast = nums[fast]
+            slow2 = nums[slow2]
 
         return slow
+
+
+
+
+
+
+      
