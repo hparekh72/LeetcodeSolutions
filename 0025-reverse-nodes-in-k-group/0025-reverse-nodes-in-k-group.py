b78f3b62daeae4ae581279d6bb37debc9655d4b6
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-04 23:21:25 +0530

    Time: 0 ms (100%), Space: 18.8 MB (16.87%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,89 +5,57 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # Iterative Approach
+        # TC: O(n)
+        # SC: O(1)
 
-        # Recursive Approach
-        # TC: O(N)
-        # SC: (N) (Recursive Stack Space)
+        if head.next == None:
+            return head
 
-        # Check if we need to reverse the group
         temp = head
-        for _ in range(k):
-            if temp == None:
-                return head
-            temp = temp.next
-
-        # Reverse the group
         prev = None
-        temp = head
 
-        for _ in range(k):
+        while temp:
+            kthNode = self.findKthNode(temp, k)
+            if kthNode == None:
+                if prev:    # If total no. of nodes less than k
+                    prev.next = temp    # Merge remaining nodes
+                break
+
+            nextNode = kthNode.next
+            kthNode.next = None
+            self.reverse(temp)
+
+            if temp == head:    # For 1st time
+                head = kthNode
+            else:
+                prev.next = kthNode
+
+            prev = temp
+            temp = nextNode
+
+        return head
+
+    def findKthNode(self, temp, k):
+        count = 1
+        while temp and count < k:
+            count += 1
+            temp = temp.next
+        return temp
+
+    def reverse(self, head):
+        temp = head
+        prev = None
+
+        while temp:
             nextNode = temp.next
             temp.next = prev
             prev = temp
             temp = nextNode
 
-        # After reverse, we know that `head` is the tail of the group.
-        # And `temp` is the next pointer in original linked list order
-        head.next = self.reverseKGroup(temp, k)
         return prev
-
-# ----------------------------------xxxxxxxxxxxxxxxxxxxxxxxx--------------------------------
-
-        # Iterative Approach
-        # TC: O(2N)
-        # SC: O(1)
-
-    #     if head.next == None:
-    #         return head
-
-    #     temp = head
-    #     prev = None
-    #     while temp != None:      
-    #         kthNode = self.findKthNode(temp, k)
-    #         if kthNode == None: 
-    #             if prev != None:  # If total number of nodes < k 
-    #                 prev.next = temp # Merge remaining nodes
-    #             break
-
-    #         nextNode = kthNode.next
-    #         kthNode.next = None
-    #         self.reverse(temp) # Reverse
-
-    #         if temp == head:
-    #             head = kthNode
-    #         else:
-    #             prev.next = kthNode
-
-    #         prev = temp
-    #         temp = nextNode
-
-    #     return head
-
-
-    # def reverse(self, head):
-    #     temp = head
-    #     prev = None
-    #     while temp != None:
-    #         nextNode = temp.next
-    #         temp.next = prev
-    #         prev = temp
-    #         temp = nextNode
-
-    #     return
-            
-    # def findKthNode(self, temp, k):
-    #     count = 1
-    #     while temp != None and count < k:
-    #         temp = temp.next
-    #         count += 1
-
-    #     return temp
-
-
-
-    
 
 
         
-    
+        
+        
