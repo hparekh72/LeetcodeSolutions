c5729e64a1e56784842ae84c80bf69544b254088
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-06 18:16:58 +0530

    Time: 43 ms (61.01%), Space: 18 MB (72.11%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -5,26 +5,33 @@
 #         self.left = left
 #         self.right = right
 
-# TC: O(N*M) N -> mainTree, M -> subTree
-# SC: O(N)
+# TC: O(n * m) m-> nodes in subTree, n-> nodes in Tree
+# SC: O(n)
 
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        # An empty subtree can always be considered a subtree of any tree.
-        if subRoot == None:
+        if not subRoot:
             return True
-        # An empty tree cannot contain any subtrees except another empty tree.
+        
         if not root:
             return False
 
-        return self.sameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-
-    def sameTree(self, newRoot, subRoot):
-
-        if newRoot == None or subRoot == None:
-            return newRoot == subRoot
-
-        return newRoot.val == subRoot.val and self.sameTree(newRoot.left, subRoot.left) and self.sameTree(newRoot.right, subRoot.right)
-
+        if self.isSameTree(root, subRoot):
+            return True
         
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    
+
+    # TC: O(m)
+    # SC: O(m)
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    
+
         
