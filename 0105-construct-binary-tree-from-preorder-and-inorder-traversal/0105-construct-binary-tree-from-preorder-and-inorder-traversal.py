cd2d12ea4dd755d99b752f6bbed65fc2e197d3ec
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-09 02:29:27 +0530

    Time: 8 ms (67.05%), Space: 19.6 MB (64.8%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,8 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+# class Solution:
+    # def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
 
         # Brute Force:
         # TC: O(N^2)
@@ -26,46 +26,59 @@ class Solution:
         # TC: O(N)
         # SC: O(N) + O(N) = O(N)
 
-        inorder_map = {} # SC: O(N)
-        for index, value in enumerate(inorder):
-            # Create a map to store indices of elements in the inorder traversal
-            inorder_map[value] = index
+    #     inorder_map = {} # SC: O(N)
+    #     for index, value in enumerate(inorder):
+    #         # Create a map to store indices of elements in the inorder traversal
+    #         inorder_map[value] = index
 
-        return self.createTree(preorder, 0, len(preorder) - 1, inorder, 0, len(inorder) - 1, inorder_map)
+    #     return self.createTree(preorder, 0, len(preorder) - 1, inorder, 0, len(inorder) - 1, inorder_map)
 
-    def createTree(self, preorder, preStart, preEnd, inorder, inStart, inEnd, inorder_map):
-        # Base case: If the start indices exceed the end indices, return None
-        if preStart > preEnd or inStart > inEnd:
-            return None
+    # def createTree(self, preorder, preStart, preEnd, inorder, inStart, inEnd, inorder_map):
+    #     # Base case: If the start indices exceed the end indices, return None
+    #     if preStart > preEnd or inStart > inEnd:
+    #         return None
 
-        # Create a new TreeNode with value at the current preorder index
-        root = TreeNode(preorder[preStart])
+    #     # Create a new TreeNode with value at the current preorder index
+    #     root = TreeNode(preorder[preStart])
 
-        # Find the index of the current root value in the inorder traversal
-        mid = inorder_map[root.val]
+    #     # Find the index of the current root value in the inorder traversal
+    #     mid = inorder_map[root.val]
 
-        # Calculate the number of elements in the left subtree
-        numsLeft = mid - inStart
+    #     # Calculate the number of elements in the left subtree
+    #     numsLeft = mid - inStart
 
-        # Recursively build the left subtree
-        root.left = self.createTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, mid - 1, inorder_map)
+    #     # Recursively build the left subtree
+    #     root.left = self.createTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, mid - 1, inorder_map)
 
-        # Recursively build the right subtree
-        root.right = self.createTree(preorder, preStart + numsLeft + 1, preEnd, inorder, mid + 1, inEnd, inorder_map)
+    #     # Recursively build the right subtree
+    #     root.right = self.createTree(preorder, preStart + numsLeft + 1, preEnd, inorder, mid + 1, inEnd, inorder_map)
 
-        return root
+    #     return root
 
+# Optimal:
+# TC: O(N)
+# SC: O(N) + O(N) = O(N)
 
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        inorder_map = {}
+        for ind, val in enumerate(inorder):
+            inorder_map[val] = ind
 
+        def createTable(preStart, preEnd, inStart, inEnd):
+            if preStart > preEnd or inStart > inEnd:
+                return None
 
+            root = TreeNode(preorder[preStart])
+            
+            mid = inorder_map[root.val]
+            numsLeft = mid - inStart
 
+            root.left = createTable(preStart + 1, preStart + numsLeft, inStart, mid - 1)
+            root.right = createTable(preStart + numsLeft + 1, preEnd, mid + 1, inEnd)
 
+            return root
 
+        return createTable(0, len(preorder) - 1, 0, len(inorder) - 1)
+ 
 
-
-
-
-    
-
-
-        
