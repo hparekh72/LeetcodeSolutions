f01e365ceaf5e5995cd8280352d7b0533339cd6f
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-12 10:21:14 +0530

    Time: 15 ms (69.36%), Space: 23.7 MB (57.25%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -1,38 +1,39 @@
+# Brute Force: Sort the array 
+# TC: __init__(): O(nlogn)
+# TC: add(): O(n) (To add an element at a specific position in an sorted array)
+# SC: O(n)
+
+# Optimal: Min Heap 
+# TC: __init__(): O(nlogk)
+# TC: add(): O(logk) (To push and pop an element in the heap)
+# SC: O(k)
+
+
+
 class KthLargest:
 
-    # Brute Force: Sort the array 
-    # TC: __init__(): O(nlogn)
-    # TC: add(): O(n) (To add an element at a specific position in an sorted array)
-    # SC: O(n)
-
-    # Optimal: Min Heap 
-    # TC: __init__(): O(nlogk)
-    # TC: add(): O(logk) (To push and pop an element in the heap)
-    # SC: O(k)
-
     def __init__(self, k: int, nums: List[int]): # O(nlogk)
-        self.heap = []
         self.k = k
-
+        self.minHeap = []
+        
         for num in nums:
-            heapq.heappush(self.heap, num) # Min Heap
+            heapq.heappush(self.minHeap, num) 
 
-            if len(self.heap) > self.k:
-                heapq.heappop(self.heap)
+            if len(self.minHeap) > k:
+                heapq.heappop(self.minHeap)
 
         return None
+        
+    def add(self, val: int) -> int: 
+        heapq.heappush(self.minHeap, val) # O(logk)
 
-    def add(self, val: int) -> int:
-        heapq.heappush(self.heap, val) # O(logk)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap) # O(logk)
 
-        if len(self.heap) > self.k:
-            heapq.heappop(self.heap) # O(logk)
-
-        return self.heap[0]
+        return self.minHeap[0]
         
 
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
-
