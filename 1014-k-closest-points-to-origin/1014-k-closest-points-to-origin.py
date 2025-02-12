1068ec4c263b11ccf72abf0a218aad95c01f1d29
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-12 11:40:00 +0530

    Time: 100 ms (17.07%), Space: 22.7 MB (55.91%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,31 +1,30 @@
-import math
+# Brute Force: Sorting
+# TC: O(nlogn)
+# SC: O(1)
+
+# Better: Using Min-Heap
+# TC: O(n + klogn)
+# SC: O(n)
+
+# Optimal: Using Max-Heap
+# TC: O(nlogk)
+# SC: O(k)
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # TC: O(nlogk)
-        # SC: O(k)
+        maxHeap = []
 
-        heap = []
+        for x, y in points:
+            dist = -(x**2 + y**2)
+            heapq.heappush(maxHeap, [dist, x, y]) # Max heap as we need the closest/smallest points in the end
+
+
+            if len(maxHeap) > k:
+                heapq.heappop(maxHeap)
+
         res = []
-
-        for point in points:
-            total = 0
-            for coordinate in point:
-                total += pow(coordinate, 2)
-            # total = math.sqrt(total) 
-            print(total, point)
-            # Max heap as we need the closest/smallest points in the end
-            heapq.heappush(heap, (-total, point)) 
-
-            if len(heap) > k:
-                heapq.heappop(heap)
-
-        for val, key in heap:
-            res.append(key)
+        while maxHeap:
+            dist, x, y = heapq.heappop(maxHeap)
+            res.append([x, y])
 
         return res
-
-
-            
-
-        
