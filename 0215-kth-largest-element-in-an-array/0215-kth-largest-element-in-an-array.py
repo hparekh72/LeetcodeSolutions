8a20e10681058a0a110d399c0e61e913be9a2f93
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-13 01:10:28 +0530

    Time: 98 ms (36.33%), Space: 28.7 MB (44.99%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,5 +1,9 @@
 # Approach 1: Using sorting
 # TC: O(nlogn)
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         nums.sort()
+#         return nums[len(nums) - k]
 
 # Approach 2: Using Min-Heap
 # TC: O(nlogk)
@@ -18,7 +22,51 @@ class Solution:
         return minHeap[0]
 
 
+# Approach 3: QuickSelect Algorithm
+# TC: Worst-case: O(n^2)
+# Average-case O(n) 
+# SC: O(n) (Recursion Stack Space)
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
 
+#         kthIndex = len(nums) - k  # When array is sorted
+
+#         def quickSelect(l, r):
+#             pivot, p = nums[r], l # Choose the pivot as the last element
+
+#             # Partitioning step
+#             for i in range(l, r):
+#                 if nums[i] <= pivot:  # Move smaller elements to the left
+#                     nums[p], nums[i] = nums[i], nums[p]
+#                     p += 1
+            
+#             # Place the pivot in its correct position
+#             nums[p], nums[r] = nums[r], nums[p]
+
+#             # Check the position of the pivot (partition index)
+#             if p > kthIndex:   # If pivot is to the right of kthIndex
+#                 return quickSelect(l, p - 1)
+#             elif p < kthIndex:   # If pivot is to the left of kthIndex
+#                 return quickSelect(p + 1, r)
+#             else:
+#                 return nums[p]
+
+#         return quickSelect(0, len(nums) - 1)
+
+
+
+
+
+            
+
+
+
+
+
+
+
+                       
+        
 
 
         
