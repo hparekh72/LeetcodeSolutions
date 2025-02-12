d56de415a699f8a8f471c973c686e7fd348abbdb
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-12 09:54:50 +0530

    Time: 74 ms (96.6%), Space: 21.3 MB (65.24%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -5,128 +5,101 @@
 #         self.left = None
 #         self.right = None
 
-# Note: We can use any traversal (Preorder, Inorder, Postorder, Level Order) to solve this problem
+
+# DFS Approach (Pre-order Traversal)
+# TC: O(n)
+# SC: O(n)
 
 # class Codec:
-    # TC: O(N)
-    # SC: O(N)
-    # def serialize(self, root):
-    #     """Encodes a tree to a single string.
+
+#     def serialize(self, root): # Pre-order Traversal # TC: O(n)
+#         """Encodes a tree to a single string.
         
-    #     :type root: TreeNode
-    #     :rtype: str
-    #     """
+#         :type root: TreeNode
+#         :rtype: str
+#         """
+#         res = []
 
-    #     # Preorder Traversal
-    #     res = []
-    #     self.preorder(root, res)
-    #     return ",".join(res)
-
-    # def preorder(self, root, res):
-    #     if root == None:
-    #         res.append("N")
-    #         return
-
-    #     res.append(str(root.val))
-    #     self.preorder(root.left, res)
-    #     self.preorder(root.right, res)
-                
-    # TC: O(N)
-    # SC: O(N)
-    # def deserialize(self, data):
-    #     """Decodes your encoded data to tree.
+#         def dfs(node):
+#             if not node:
+#                 res.append("N")
+#                 return 
+            
+#             res.append(str(node.val))
+#             dfs(node.left)
+#             dfs(node.right)
+#         dfs(root)
+#         return ",".join(res)
         
-    #     :type data: str
-    #     :rtype: TreeNode
-    #     """
-    #     arr = data.split(",")
-    #     pos = [0]
-    #     return self.buildTree(arr, pos)
 
-    
-    # def buildTree(self, arr, pos): # DFS traversal
-    #     if arr[pos[0]] == "N":
-    #         pos[0] += 1
-    #         return None
+#     def deserialize(self, data): # TC: O(n)
+#         """Decodes your encoded data to tree.
+        
+#         :type data: str
+#         :rtype: TreeNode
+#         """
+#         vals = data.split(",")
+#         self.i = 0
 
-    #     node = TreeNode(int(arr[pos[0]]))
-    #     pos[0] += 1
-    #     node.left = self.buildTree(arr, pos)
-    #     node.right = self.buildTree(arr, pos)
-    #     return node
-    
-    
+#         def dfs():
+#             if vals[self.i] == "N":
+#                 self.i += 1
+#                 return None
+
+#             node = TreeNode(int(vals[self.i]))
+#             self.i += 1
+#             node.left = dfs()
+#             node.right = dfs()
+#             return node
+
+#         return dfs()
+
+# BFS Approach
+
+# TC: O(n)
+# SC: O(n)
+
 class Codec:
-    # TC: O(N)
-    # SC: O(N)
     def serialize(self, root):
-        # Level Order Traversal (BFS)
-        if root == None:
-            return ""
+        if not root:
+            return "N"
 
-        s = ""
-        queue = [root]
+        res = []
+        queue = deque([root])
         while queue:
-            node = queue.pop(0)
-
-            # If node is None, append "N" to the string
-            if node == None: 
-                s += "N,"
+            node = queue.popleft()
+            if not node:
+                res.append("N")
             else:
-                s += str(node.val) + ","
-
-                # Push the left and right children to the queue for further traversal
+                res.append(str(node.val))
                 queue.append(node.left)
                 queue.append(node.right)
-        return s
-
-    # TC: O(N)
-    # SC: O(N)
-    def deserialize(self, data):
-        # Level Order Traversal
-
-        if not data:
+        return ",".join(res)
+    
+    def deserialize(self, data): 
+        vals = data.split(",")
+        if vals[0] == "N":
             return None
+        
+        root = TreeNode(vals[0])
+        queue = deque([root])
+        index = 1
 
-        arr = data.split(",")
-
-        # Read the root value from the serialized data
-        root = TreeNode(int(arr.pop(0)))
-        queue = [root]
-
-        # Perform level-order traversal to reconstruct the tree
         while queue:
-            node = queue.pop(0)
-
-            # Read the value of the left child from the serialized data
-            left_val = arr.pop(0)
-            # If the value is not "N", create a new left child and push it to the queue
-            if left_val != "N":
-                node.left = TreeNode(int(left_val))
+            node = queue.popleft()
+            if vals[index] != "N":
+                node.left = TreeNode(vals[index])
                 queue.append(node.left)
+            index += 1
 
-            # Read the value of the right child from the serialized data
-            right_val = arr.pop(0)
-            # If the value is not "N", create a new right child and push it to the queue
-            if right_val != "N":
-                node.right = TreeNode(int(right_val))
+            if vals[index] != "N":
+                node.right = TreeNode(vals[index])
                 queue.append(node.right)
+            index += 1
 
         return root
 
         
-
-            
-
-        
-        
-
-
-
-    
-
-
-
 
 
 
