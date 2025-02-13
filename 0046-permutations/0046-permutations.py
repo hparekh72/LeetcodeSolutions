c55fcf0eae774f4f0e8b1de068e268169b5aee19
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-13 15:38:56 +0530

    Time: 0 ms (100%), Space: 17.9 MB (53.77%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,36 +1,35 @@
-
 """
 Brute Force:
 TC: O(n! * n(looping)) 
-SC: O(n) (Recursive stack space) + O(n) (ds array) + O(n)(visited array) + O(n!)(res array)
+SC: O(n) (Recursive stack space) + O(n) (ds array) + O(n)(visited array) + O(n! * n)(res array)
 """
 
-# class Solution:
-#     def permute(self, nums: List[int]) -> List[List[int]]:
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
 
-#         res = []
-#         visited = [False]*len(nums)
-#         self.getPermutations(nums, visited, [], res)
-#         return res
+        res = []
+        visited = [False]*len(nums)
+        self.getPermutations(nums, visited, [], res)
+        return res
 
-#     def getPermutations(self, nums, visited, ds, res):
+    def getPermutations(self, nums, visited, ds, res):
 
-#         # Base Case
-#         if len(ds) == len(nums):
-#             res.append(ds.copy())
-#             return
+        # Base Case
+        if len(ds) == len(nums):
+            res.append(ds.copy())
+            return
 
 
-#         for i in range(len(nums)):
-#             if not visited[i]:
+        for i in range(len(nums)):
+            if not visited[i]:
 
-#                 visited[i] = True
-#                 ds.append(nums[i])
+                visited[i] = True
+                ds.append(nums[i])
 
-#                 self.getPermutations(nums, visited, ds, res)
+                self.getPermutations(nums, visited, ds, res)
 
-#                 visited[i] = False
-#                 ds.pop()
+                visited[i] = False
+                ds.pop()
 
 """
 Optimal: Swapping
@@ -38,33 +37,25 @@ TC: O(n! * n(looping))
 SC: O(n) (Recursive stack space) + O(n!)(res array)
 """
 
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        self.getPermutations(nums, 0, res)
-        return res
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         self.getPermutations(nums, 0, res)
+#         return res
 
-    def swap(self, i, j, nums):
-        temp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = temp
+#     def swap(self, i, j, nums):
+#         temp = nums[i]
+#         nums[i] = nums[j]
+#         nums[j] = temp
 
-    def getPermutations(self, nums, index, res):
+#     def getPermutations(self, nums, index, res):
 
-        # Base Case:
-        if index == len(nums):
-            res.append(nums.copy())
-            return 
+#         # Base Case:
+#         if index == len(nums):
+#             res.append(nums.copy())
+#             return 
 
-        for i in range(index, len(nums)):
-            self.swap(index, i, nums)
-            self.getPermutations(nums, index + 1, res)
-            self.swap(index, i, nums) # Backtrack
-
-
-
-
-
-
-     
-        
+#         for i in range(index, len(nums)):
+#             self.swap(index, i, nums)
+#             self.getPermutations(nums, index + 1, res)
+#             self.swap(index, i, nums) # Backtrack
