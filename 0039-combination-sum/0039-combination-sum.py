8e23faa2b420db33ef23c4f91f6bb6e967119b91
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-13 13:37:24 +0530

    Time: 11 ms (61.13%), Space: 17.7 MB (83.99%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,31 +1,26 @@
+# TC: O(2 ^ t/m) * k, where t is the target, m is the minimum value in nums and k is the average combination size.
+
+# SC: O(t/m) (Recursion Stack Space)
+
 class Solution:
-    # TC: (Exponential) O(2^T * k)
-    # Here T is how many times the smallest candidate can be used to reach or exceed the target, and k is the average combination size.
-
-    # SC : O(T/min(candidates)) + O(2^T * k)
-    # Here, both the recursion stack depth and the storage for all valid combinations.
-
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        self.solve(candidates, target, 0, [], res)
+        self.solve(0, candidates, target, [], res)
         return res
 
-    def solve(self, candidates, target, index, ds, res):
-
-        # Base Case 
-        if index == len(candidates):
+    def solve(self, ind, candidates, target, ds, res):
+        # Base Case
+        if ind >= len(candidates):
             if target == 0:
-                res.append(ds[:]) # or res.append(list(ds)) 
+                res.append(ds.copy())
             return 
 
         # Pick
-        if target - candidates[index] >= 0:
-            ds.append(candidates[index])
-            self.solve(candidates, target - candidates[index], index, ds, res) 
+        if target - candidates[ind] >= 0:
+            ds.append(candidates[ind])
+            self.solve(ind, candidates, target - candidates[ind], ds, res)
             ds.pop()
+        # Not Pick
 
-        # Not Pick  
-        self.solve(candidates, target, index + 1, ds, res) 
-
-        
+        self.solve(ind + 1, candidates, target, ds, res)
         
