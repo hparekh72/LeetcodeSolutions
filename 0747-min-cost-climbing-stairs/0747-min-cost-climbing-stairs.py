360d5ba836b99a82ad913465251fd7fcc78767f5
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-22 23:23:55 -0800

    Time: 4 ms (40.16%), Space: 18 MB (30.87%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -4,7 +4,9 @@ class Solution:
         dp = [-1 for _ in range(n + 1)]
 
         # return self.solveUsingRecursion(n, cost) # Start from "step n" (beyond the last step)
-        return self.solveUsingMemoization(n, cost, dp)
+        # return self.solveUsingMemoization(n, cost, dp)
+        # return self.solveUsingTabulation(n, cost)
+        return self.solveUsingSpaceOptimized(n, cost)
 
     
     # TC: O(2^n)
@@ -28,6 +30,36 @@ class Solution:
         # Choose the cheaper step before reaching here
         dp[ind] = min(self.solveUsingMemoization(ind - 1, cost, dp) + cost[ind - 1], self.solveUsingMemoization(ind - 2, cost, dp) + cost[ind - 2])
         return dp[ind]
+
+
+    # TC: O(n)
+    # SC: O(n) (dp array)
+    def solveUsingTabulation(self, n, cost):
+        dp = [0 for _ in range(n + 1)]
+        # dp[0], dp[1] = 0, 0
+
+        for ind in range(2, n + 1):
+            dp[ind] = min(dp[ind - 1] + cost[ind - 1], dp[ind - 2] + cost[ind - 2])
+        
+        return dp[n]
+
+
+    # TC: O(n)
+    # SC: O(1) 
+    def solveUsingSpaceOptimized(self, n, cost):
+        prev1, prev2 = 0,0 
+
+        curr = prev1
+        for ind in range(2, n + 1):
+            curr = min(prev1 + cost[ind - 1], prev2 + cost[ind - 2])
+            prev2 = prev1
+            prev1 = curr
+        
+        return curr
+ 
+
+
+
         
 
 
