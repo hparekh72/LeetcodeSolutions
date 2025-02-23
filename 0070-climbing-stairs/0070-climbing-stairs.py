ca99a1fd38b15f200ecea2c8389814a6fc313b46
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-22 18:27:58 -0800

    Time: 0 ms (100%), Space: 18 MB (8.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -15,13 +15,12 @@ class Solution:
 
         def climbStairs(self, n: int) -> int: # Memoization
             dp = [-1 for _ in range(n + 1)]
-            return self.solveUsingMemoization(n, dp)
+            # return self.solveUsingMemoization(n, dp)
+            # return self.solveUsingTabulation(n, dp)
+            return self.spaceOptimized(n)
 
         def solveUsingMemoization(self, n, dp):
-            if n < 0:
-                return 0
-
-            if n == 0:
+            if n <= 1:
                 return 1
             
             if dp[n] != -1:
@@ -29,6 +28,37 @@ class Solution:
 
             dp[n] = self.solveUsingMemoization(n - 1, dp) + self.solveUsingMemoization(n - 2, dp)
             return dp[n]
+
+        def solveUsingTabulation(self, n, dp):
+            dp[0] = 1
+            dp[1] = 1
+
+            for i in range(2, n + 1):
+                dp[i] = dp[i - 1] + dp[i - 2]
+
+            return dp[n]
+
+        def spaceOptimized(self, n):
+            prev2 = 1
+            prev = 1
+
+            curr = prev
+            for i in range(2, n + 1):
+                curr = prev + prev2
+                prev2 = prev
+                prev = curr
+            
+            return curr
+                
+
+
+        
+
+
+
+            
+
+
 
         
 
