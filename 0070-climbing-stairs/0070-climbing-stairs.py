9424dcec2f0c4661e94027f8a73a64c36f4e2611
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-02-22 18:12:15 -0800

    Time: 0 ms (100%), Space: 17.9 MB (8.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,45 +1,44 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        dp = [-1 for _ in range(n + 1)]
-        # return self.solveUsingMemoization(n, dp)
-        # return self.solveUsingTabulation(n, dp)
-        return self.solveUsingSpaceOptimization(n)
+    # def climbStairs(self, n: int) -> int: # Recursion
 
-
-    
-    # TC: O(n) 
-    # SC: O(n) (dp array and recursion stack space)
-    def solveUsingMemoization(self, n, dp):
-        if n <= 1:
-            return 1
+    #     # TC: O(2^n)
+    #     # SC: O(n)
         
-        if dp[n] != -1:
+    #     # Base Case 
+    #     if n < 0:
+    #         return 0
+
+    #     if n == 0:
+    #         return 1 
+
+    #     return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+        def climbStairs(self, n: int) -> int: # Memoization
+            dp = [-1 for _ in range(n + 1)]
+            return self.solveUsingMemoization(n, dp)
+
+        def solveUsingMemoization(self, n, dp):
+            if n < 0:
+                return 0
+
+            if n == 0:
+                return 1
+            
+            if dp[n] != -1:
+                return dp[n]
+
+            dp[n] = self.solveUsingMemoization(n - 1, dp) + self.solveUsingMemoization(n - 2, dp)
             return dp[n]
 
-        dp[n] = self.solveUsingMemoization(n - 1, dp) + self.solveUsingMemoization(n - 2, dp)
-        return dp[n]
-
-    # TC: O(n) 
-    # SC: O(n) (dp array)
-    def solveUsingTabulation(self, n, dp):
-        dp[0], dp[1] = 1, 1
-
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-
-        return dp[n]
-
-    def solveUsingSpaceOptimization(self, n):
-        prev2 = 1
-        prev = 1
         
-        curr = prev
-        for i in range(2, n + 1):
-            curr = prev + prev2
-            prev2 = prev
-            prev = curr
 
-        return curr
+        
+
+
+
+
+
+ 
             
 
 
