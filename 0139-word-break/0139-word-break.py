6a0297733f804efad7ec081fbf940ecf24e9aa81
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-03-02 17:05:49 -0800

    Time: 0 ms (100%), Space: 17.8 MB (87.49%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,49 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # return self.solveUsingRecursion(0, s, wordDict)
+
+        dp = [-1 for _ in range(len(s) + 1)]
+        return self.solveUsingMemoization(0, s, wordDict, dp)
+    
+    # TC: O(2^n * m)
+    # SC: O(n) (Recursion Stack Space)
+    def solveUsingRecursion(self, ind, s, wordDict):
+        
+        if ind == len(s): # Base case: reached the end
+            return True
+
+        for w in wordDict: # Try each word from dictionary
+            if (ind + len(w)) <= len(s) and s[ind : ind + len(w)] == w:  # Check if within bounds and substring matches
+                if self.solveUsingRecursion(ind + len(w), s, wordDict): # Recursively check remaining part
+                    return True
+        return False  # No valid segmentation found
+
+
+
+    # TC: O(n * m)
+    # SC: O(n) (Dp and Recursion Stack Space)
+    def solveUsingMemoization(self, ind, s, wordDict, dp):
+        if ind == len(s):
+            return True
+
+        if dp[ind] != -1:
+            return dp[ind]
+
+        for w in wordDict:
+            if (ind + len(w)) <= len(s) and s[ind : ind + len(w)] == w:  # Check if within bounds and substring matches
+                if self.solveUsingMemoization(ind + len(w), s, wordDict, dp): # Recursively check remaining part
+                    dp[ind] = True
+                    return dp[ind]
+
+        dp[ind] = False
+        return dp[ind]
+
+
+
+
+    
+
+
+            
+        
+        
