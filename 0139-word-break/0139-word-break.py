75b9cef41af4fc6fa7cee2b7f1ae83dca0358ff5
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-03-02 17:20:32 -0800

    Time: 0 ms (100%), Space: 18 MB (25.93%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -2,8 +2,10 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         # return self.solveUsingRecursion(0, s, wordDict)
 
-        dp = [-1 for _ in range(len(s) + 1)]
-        return self.solveUsingMemoization(0, s, wordDict, dp)
+        # dp = [-1 for _ in range(len(s) + 1)]
+        # return self.solveUsingMemoization(0, s, wordDict, dp)
+
+        return self.solveUsingTabulation(s, wordDict)
     
     # TC: O(2^n * m)
     # SC: O(n) (Recursion Stack Space)
@@ -37,6 +39,26 @@ class Solution:
 
         dp[ind] = False
         return dp[ind]
+
+
+    # TC: O(n * m)
+    # SC: O(n) (Dp)
+    def solveUsingTabulation(self, s, wordDict):
+        n = len(s)
+
+        dp = [False for _ in range(len(s) + 1)]
+        dp[n] = True
+
+        for ind in range(n - 1, -1, -1):
+            for w in wordDict:
+                if (ind + len(w)) <= len(s) and s[ind : ind + len(w)] == w:  # Check if within bounds and substring matches
+                    if dp[ind + len(w)]:
+                        dp[ind] = True
+                        break
+
+        return dp[0]
+    
+
 
 
 
