088e7de6b365e36c89f56c0d7d0d2cce8fdb2b12
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-03-03 19:13:05 -0800

    Time: 1623 ms (5.06%), Space: 18.7 MB (33.15%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,13 +3,18 @@ class Solution:
         n = len(coins)
         # ans = self.solveUsingRecursion(n - 1, coins, amount)
 
-        dp = [[-1 for _ in range(amount + 1)] for _ in range(n)]
-        ans = self.solveUsingMemoization(n - 1, coins, amount, dp)
+        # dp = [[-1 for _ in range(amount + 1)] for _ in range(n)]
+        # ans = self.solveUsingMemoization(n - 1, coins, amount, dp)
+
+        # ans = self.solveUsingTabulation(coins, amount)
+
+        ans = self.solveUsingSpaceOptimization(coins, amount)
 
         if ans == float('inf'):
             return -1
         else:
             return ans
+
 
     def solveUsingRecursion(self, ind, coins, amount):
         if ind == 0:
@@ -48,6 +53,50 @@ class Solution:
         
         dp[ind][amount] = min(pick, notPick)
         return dp[ind][amount]
+
+    def solveUsingTabulation(self, coins, amount):
+        n = len(coins)
+        dp = [[float('inf') for _ in range(amount + 1)] for _ in range(n)]
+
+        # Base Case
+        for num in range(amount + 1):
+            if num % coins[0] == 0:
+                dp[0][num] = num // coins[0]
+
+        for ind in range(1, n):
+            for num in range(amount + 1):
+                notPick = 0 + dp[ind - 1][num]
+
+                pick = float('inf')
+                if coins[ind] <= num: 
+                    pick = 1 + dp[ind][num - coins[ind]] 
+                
+                dp[ind][num] = min(pick, notPick)
+        return dp[n - 1][amount]  
+
+    def solveUsingSpaceOptimization(self, coins, amount):
+        n = len(coins)
+        prev = [float('inf') for _ in range(amount + 1)] 
+
+        # Base Case
+        for num in range(amount + 1):
+            if num % coins[0] == 0:
+                prev[num] = num // coins[0]
+
+        for ind in range(1, n):
+            curr = [float('inf') for _ in range(amount + 1)] 
+            for num in range(amount + 1):
+                notPick = 0 + prev[num]
+
+                pick = float('inf')
+                if coins[ind] <= num: 
+                    pick = 1 + curr[num - coins[ind]] 
+                
+                curr[num] = min(pick, notPick)
+            prev = curr
+        return prev[amount]                   
+
+
 
         
 
