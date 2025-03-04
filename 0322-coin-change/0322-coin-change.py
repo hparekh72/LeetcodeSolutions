96a9a36ed9fec2a4331115613564476ea217be8b
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-03-03 19:13:14 -0800

    Time: 1637 ms (5.01%), Space: 18.8 MB (33.15%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,105 +1,128 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        # ans = self.solveUsingRecursion(n - 1, coins, amount)
+        # ans = self.solveUsingRecursion(n - 1, amount, coins)
+        # if ans == float('inf'):
+        #     return -1
+        # else:
+        #     return ans      
 
         # dp = [[-1 for _ in range(amount + 1)] for _ in range(n)]
-        # ans = self.solveUsingMemoization(n - 1, coins, amount, dp)
 
-        # ans = self.solveUsingTabulation(coins, amount)
+        # ans = self.solveUsingMemoization(n - 1, amount, coins, dp)
+        # if ans == float('inf'):
+        #     return -1
+        # else:
+        #     return ans
 
-        ans = self.solveUsingSpaceOptimization(coins, amount)
 
+        # ans = self.solveUsingTabulation(amount, coins, n)
+        # if ans == float('inf'):
+        #     return -1
+        # else:
+        #     return ans
+
+        ans = self.solveUsingSpaceOptimization(amount, coins, n)
         if ans == float('inf'):
             return -1
         else:
             return ans
 
-
-    def solveUsingRecursion(self, ind, coins, amount):
+    # TC: O(2^n) (exponential)
+    # SC: O(amount) (recursion stack space)
+    def solveUsingRecursion(self, ind, amount, coins):
+        # Base Case
         if ind == 0:
             if amount % coins[ind] == 0:
                 return amount // coins[ind]
             else:
                 return float('inf')
-            
 
-        notPick = 0 + self.solveUsingRecursion(ind - 1, coins, amount)
+        # Not Pick
+        notPick = 0 + self.solveUsingRecursion(ind - 1, amount, coins)
 
+        # Pick
         pick = float('inf')
-        if amount >= coins[ind]: 
-            pick = 1 + self.solveUsingRecursion(ind, coins, amount - coins[ind])
-        
-        return min(pick, notPick)
+        if coins[ind] <= amount:
+            pick = 1 + self.solveUsingRecursion(ind, amount - coins[ind], coins)
 
+        return min(notPick, pick)
 
-    # TC: O(amount * n) 
+    # TC: O(amount * n)
     # SC: O(amount * n) + O(amount) (recursion stack space)
-    def solveUsingMemoization(self, ind, coins, amount, dp):
+    def solveUsingMemoization(self, ind, amount, coins, dp):
+        # Base Case
         if ind == 0:
             if amount % coins[ind] == 0:
                 return amount // coins[ind]
             else:
                 return float('inf')
-        
+
         if dp[ind][amount] != -1:
             return dp[ind][amount]
-            
-        notPick = 0 + self.solveUsingMemoization(ind - 1, coins, amount, dp)
 
+        # Not Pick
+        notPick = 0 + self.solveUsingMemoization(ind - 1, amount, coins, dp)
+
+        # Pick
         pick = float('inf')
-        if amount >= coins[ind]: 
-            pick = 1 + self.solveUsingMemoization(ind, coins, amount - coins[ind], dp)
-        
-        dp[ind][amount] = min(pick, notPick)
+        if coins[ind] <= amount:
+            pick = 1 + self.solveUsingMemoization(ind, amount - coins[ind], coins, dp)
+
+        dp[ind][amount] = min(notPick, pick)
         return dp[ind][amount]
 
-    def solveUsingTabulation(self, coins, amount):
-        n = len(coins)
+
+    # TC: O(amount * n)
+    # SC: O(amount * n) 
+
+    def solveUsingTabulation(self, amount, coins, n):
         dp = [[float('inf') for _ in range(amount + 1)] for _ in range(n)]
 
         # Base Case
         for num in range(amount + 1):
-            if num % coins[0] == 0:
+            if num % coins[0] == 0: 
                 dp[0][num] = num // coins[0]
 
         for ind in range(1, n):
             for num in range(amount + 1):
-                notPick = 0 + dp[ind - 1][num]
+                # Not Pick
+                notPick = 0 + dp[ind - 1][num] 
 
+                # Pick
                 pick = float('inf')
-                if coins[ind] <= num: 
-                    pick = 1 + dp[ind][num - coins[ind]] 
-                
-                dp[ind][num] = min(pick, notPick)
-        return dp[n - 1][amount]  
+                if coins[ind] <= num:
+                    pick = 1 + dp[ind][num - coins[ind]]
 
-    def solveUsingSpaceOptimization(self, coins, amount):
-        n = len(coins)
-        prev = [float('inf') for _ in range(amount + 1)] 
+                dp[ind][num] = min(notPick, pick)
+
+        return dp[n - 1][amount]
+
+
+    # TC: O(amount * n)
+    # SC: O(amount) 
+
+    def solveUsingSpaceOptimization(self, amount, coins, n): # Two Array (Space Optimized)
+        prev = [float('inf') for _ in range(amount + 1)]
 
         # Base Case
         for num in range(amount + 1):
-            if num % coins[0] == 0:
+            if num % coins[0] == 0: 
                 prev[num] = num // coins[0]
 
         for ind in range(1, n):
-            curr = [float('inf') for _ in range(amount + 1)] 
+            curr = [float('inf') for _ in range(amount + 1)]
             for num in range(amount + 1):
-                notPick = 0 + prev[num]
+                # Not Pick
+                notPick = 0 + prev[num] 
 
+                # Pick
                 pick = float('inf')
-                if coins[ind] <= num: 
-                    pick = 1 + curr[num - coins[ind]] 
-                
-                curr[num] = min(pick, notPick)
+                if coins[ind] <= num:
+                    pick = 1 + curr[num - coins[ind]]
+
+                curr[num] = min(notPick, pick)
+            
             prev = curr
-        return prev[amount]                   
 
-
-
-        
-
-    
-
-
+        return prev[amount]
