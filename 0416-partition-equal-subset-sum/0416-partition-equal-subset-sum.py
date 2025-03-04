1f51454c1a6001fab488db0a46d9bdb117f71344
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-03-03 20:16:52 -0800

    Time: 1799 ms (20.88%), Space: 34 MB (37.52%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -14,8 +14,10 @@ class Solution:
             target = total // 2
             # return self.solveUsingRecursion(n - 1, target, nums)
 
-            dp = [[-1 for _ in range(target + 1)] for _ in range(n)]
-            return self.solveUsingMemoization(n - 1, target, nums, dp)
+            # dp = [[-1 for _ in range(target + 1)] for _ in range(n)]
+            # return self.solveUsingMemoization(n - 1, target, nums, dp)
+
+            return self.solveUsingTabulation(target, nums)
         
     def solveUsingRecursion(self, ind, target, nums):
 
@@ -35,6 +37,9 @@ class Solution:
         
         return pick or notPick
 
+
+    # TC: O(n * target)
+    # SC: O(n * target) + O(target) (recursion stack space)
     def solveUsingMemoization(self, ind, target, nums, dp):
         # Base Case
         if ind == 0:
@@ -49,12 +54,42 @@ class Solution:
         # Not Pick
         notPick = self.solveUsingMemoization(ind - 1, target, nums, dp)
 
+        # Pick
         pick = False
         if nums[ind] <= target:
             pick = self.solveUsingMemoization(ind - 1, target - nums[ind], nums, dp)
         
         dp[ind][target] = pick or notPick
         return dp[ind][target]
+
+    # TC: O(n * target)
+    # SC: O(n * target) 
+    def solveUsingTabulation(self, target, nums):
+        n = len(nums)
+
+        dp = [[False for _ in range(target + 1)] for _ in range(n)]
+
+        # Base Case
+        if nums[0] == target:
+            dp[0][nums[0]] = True
+
+        for ind in range(n):
+            dp[ind][0] = True
+
+        for ind in range(n):
+            for num in range(target + 1):
+                # Not Pick
+                notPick = dp[ind - 1][num] 
+
+                # Pick
+                pick = False
+                if nums[ind] <= num:
+                    pick = dp[ind - 1][num - nums[ind]] 
+                
+                dp[ind][num] = pick or notPick
+        return dp[n - 1][target]
+        
+
 
         
 
