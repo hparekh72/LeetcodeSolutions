095ec04a8ba54f49c84af16ad09e410d31b73711
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2025-03-03 17:22:18 -0800

    Time: 3 ms (96.23%), Space: 18.5 MB (9.28%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -16,7 +16,10 @@ class Solution:
 
         # return self.solveUsingTabulation(nums)
 
-        return self.solveUsingSpaceOptimization(nums)
+        # return self.solveUsingSpaceOptimization(nums)
+        # return self.solveLIS(nums)
+        # return self.printLIS(nums)
+        return self.LISUsingBinarySearch(nums)
 
     def solveUsingRecursion(self, ind, prev_ind, nums):
         if ind == len(nums):
@@ -92,33 +95,67 @@ class Solution:
 
         return front[-1 + 1]
 
-    
-        
+    def solveLIS(self, nums):
+        n = len(nums)
+        dp = [1 for _ in range(n)]
+
+        longest = 0
+        for ind in range(n):
+            for prev_ind in range(ind):
+                if nums[prev_ind] < nums[ind]:
+                    dp[ind] = max(dp[ind], dp[prev_ind] + 1)
+            longest = max(longest, dp[ind])
+
+        return longest
+
+    def printLIS(self, nums):
+            n = len(nums)
+            dp = [1 for _ in range(n)]
+            hashArr = []
+            last_index = 0
+
+            longest = 0
+            for ind in range(n):
+                hashArr.append(ind)
+                for prev_ind in range(ind):
+                    if nums[prev_ind] < nums[ind] and (dp[prev_ind] + 1) > dp[ind]:
+                        dp[ind] = dp[prev_ind] + 1
+                        hashArr[ind] = prev_ind
+                if dp[ind] > longest:
+                    longest = dp[ind]
+                    last_index = ind
+
+            # Backtrack
+            res = []
+            res.append(nums[last_index])
+            while hashArr[last_index] != last_index:
+                last_index = hashArr[last_index]
+                res.append(nums[last_index])
+
+            res.reverse()
+            # print(res)
+
+            return longest
+
+    def LISUsingBinarySearch(self, nums):
+        temp = []
+        lengthOfLIS = 0
+
+        for num in nums:
+            if len(temp) == 0 or num > temp[-1]:
+                temp.append(num)
+                lengthOfLIS += 1
+            else:
+                index = bisect.bisect_left(temp, num)
+                temp[index] = num
+
+        return lengthOfLIS
+
+
+
+                
+
 
     
 
-
-
-        
-
-
-
-
-
-            
-
-
-
-
-        
-        
- 
-
-        
-
-
-
-
-
-
-        
+    
